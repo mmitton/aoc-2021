a45c496ecbda4f1c0e8a4409807090cec00f86ad
Michael Conrad <mmitton@gmail.com>
2021-12-20 09:02:12 -0500

    2017 - Day 2

---
+++ 2017/day-02/day-02-part-1/src/main.rs
@@ -0,0 +1,74 @@
+#[cfg(debug_assertions)]
+const INPUT_FILE: &str = "../input-sample.txt";
+#[cfg(not(debug_assertions))]
+const INPUT_FILE: &str = "../input.txt";
+
+#[derive(Debug)]
+enum Error {
+    IO(std::io::Error),
+    NAN(std::num::ParseIntError),
+}
+
+#[derive(Debug)]
+struct Spreadsheet {
+    rows: Vec<Vec<isize>>,
+}
+
+impl Spreadsheet {
+    fn checksum(&self) -> isize {
+        let mut sum = 0;
+        for row in &self.rows {
+            let mut min = row[0];
+            let mut max = row[1];
+            for cell in row {
+                if *cell < min {
+                    min = *cell
+                }
+                if *cell > max {
+                    max = *cell
+                }
+            }
+            sum += max - min
+        }
+
+        sum
+    }
+}
+
+fn load_input(filename: &str) -> Result<Spreadsheet, Error> {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+    let f = File::open(filename).map_err(|e| Error::IO(e))?;
+
+    let lines = BufReader::new(f).lines();
+
+    let mut spreadsheet = Spreadsheet { rows: Vec::new() };
+
+    for line in lines {
+        let line = line.map_err(|e| Error::IO(e))?;
+        let line = line.trim();
+        if line == "" {
+            continue;
+        }
+        if line.starts_with("#") {
+            continue;
+        }
+
+        let mut values: Vec<isize> = Vec::new();
+        for val in line.split("\t") {
+            values.push(val.parse().map_err(|e| Error::NAN(e))?);
+        }
+
+        spreadsheet.rows.push(values);
+    }
+
+    Ok(spreadsheet)
+}
+
+fn main() -> Result<(), Error> {
+    let spreadsheet = load_input(INPUT_FILE)?;
+
+    println!("Checksum: {}", spreadsheet.checksum());
+
+    Ok(())
+}
