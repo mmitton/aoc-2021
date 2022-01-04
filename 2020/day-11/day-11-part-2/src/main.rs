c56baf78b34d12d80ec8f140f70f7c371d4a7bff
Michael Conrad <mmitton@gmail.com>
2022-01-04 11:55:38 -0500

    2020 - Day 11

---
+++ 2020/day-11/day-11-part-2/src/main.rs
@@ -0,0 +1,115 @@
+#[cfg(debug_assertions)]
+const INPUT_FILE: &str = "../input-sample.txt";
+#[cfg(not(debug_assertions))]
+const INPUT_FILE: &str = "../input.txt";
+
+#[derive(Debug)]
+enum Error {
+    IO(std::io::Error),
+}
+
+fn load_input(filename: &str) -> Result<Vec<Vec<char>>, Error> {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+    let f = File::open(filename).map_err(|e| Error::IO(e))?;
+
+    let lines = BufReader::new(f).lines();
+
+    let mut map = Vec::new();
+
+    for line in lines {
+        let line = line.map_err(|e| Error::IO(e))?;
+        let line = line.trim().to_string();
+        if line == "" || line.starts_with("#") {
+            continue;
+        }
+
+        map.push(line.chars().collect());
+    }
+
+    Ok(map)
+}
+
+fn print(map: &Vec<Vec<char>>) {
+    let mut occupied = 0;
+    for y in 0..map.len() {
+        for x in 0..map[0].len() {
+            let c = map[y][x];
+            if c == '#' {
+                occupied += 1;
+            }
+            print!("{}", c);
+        }
+        println!();
+    }
+    println!("{} seats occupied\n", occupied);
+}
+
+fn main() -> Result<(), Error> {
+    let mut map = load_input(INPUT_FILE)?;
+
+    let mut flips: Vec<(usize, usize)> = Vec::new();
+    let mut changed = true;
+    while changed {
+        if cfg!(debug_assertions) {
+            print(&map);
+        }
+        flips.clear();
+
+        for y in 0..map.len() as isize {
+            for x in 0..map[0].len() as isize {
+                let mut occupied = 0;
+                for dy in -1..=1 {
+                    'seat_loop: for dx in -1..=1 {
+                        if dx == 0 && dy == 0 {
+                            continue;
+                        }
+
+                        for i in 1..isize::MAX {
+                            let x = x + (i * dx);
+                            let y = y + (i * dy);
+                            if x < 0 || y < 0 {
+                                continue 'seat_loop;
+                            }
+
+                            let x = x as usize;
+                            let y = y as usize;
+                            if x == map[0].len() || y == map.len() {
+                                continue 'seat_loop;
+                            }
+
+                            if map[y][x] == '#' {
+                                occupied += 1;
+                                break;
+                            } else if map[y][x] == 'L' {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                let x = x as usize;
+                let y = y as usize;
+                if map[y][x] == 'L' && occupied == 0 {
+                    flips.push((x, y));
+                } else if map[y][x] == '#' && occupied >= 5 {
+                    flips.push((x, y));
+                }
+            }
+        }
+
+        changed = flips.len() != 0;
+        for flip in &flips {
+            if map[flip.1][flip.0] == '#' {
+                map[flip.1][flip.0] = 'L';
+            } else if map[flip.1][flip.0] == 'L' {
+                map[flip.1][flip.0] = '#';
+            } else {
+                panic!();
+            }
+        }
+    }
+
+    print(&map);
+    Ok(())
+}
