e571bc070c338aba4e1b4c4a26b6ddeb95ea88c1
Michael Conrad <mmitton@gmail.com>
2022-12-07 07:46:10 -0500

    2022 - Day 7 - Completed

--- 2022/day-07/day-07-part-2/src/main.rs
+++ 2022/day-07/day-07-part-2/src/main.rs
@@ -3,122 +3,49 @@ const INPUT_FILE: &str = "../input-sample.txt";
 #[cfg(not(debug_assertions))]
 const INPUT_FILE: &str = "../input.txt";
 
-use std::collections::BTreeMap;
-use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-struct Dir {
-    name: String,
-    files: BTreeMap<String, usize>,
-    dirs: BTreeMap<String, Dir>,
+#[derive(Debug)]
+struct Files {
+    entries: Vec<(String, bool, usize)>,
 }
 
-impl fmt::Debug for Dir {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Dir")
-            .field("size", &self.size())
-            .field("files", &self.files)
-            .field("dirs", &self.dirs)
-            .finish()
-    }
-}
-
-impl Dir {
-    fn new(name: &str) -> Self {
+impl Files {
+    fn new() -> Self {
         Self {
-            name: name.into(),
-            files: BTreeMap::new(),
-            dirs: BTreeMap::new(),
+            entries: Vec::new(),
         }
     }
 
-    fn size(&self) -> usize {
-        let mut size = 0;
-        for dir in self.dirs.values() {
-            size += dir.size();
-        }
-        for file in self.files.values() {
-            size += file;
-        }
-        size
-    }
+    fn add(&mut self, name: &str, size: usize) {
+        let parts: Vec<&str> = name.split('/').collect();
 
-    fn part1(&self) -> usize {
-        let mut ans = 0;
+        for i in 0..parts.len() {
+            let name = format!("/{}", parts[1..i + 1].join("/"));
 
-        let my_size = self.size();
-        if my_size < 100_000 {
-            ans += my_size;
-        }
-
-        for dir in self.dirs.values() {
-            ans += dir.part1();
-        }
-
-        ans
-    }
-
-    fn part2(&self, need: usize) -> Vec<(usize, String)> {
-        let mut ret = Vec::new();
-        if self.size() >= need {
-            ret.push((self.size(), self.name.clone()));
-        }
-
-        for dir in self.dirs.values() {
-            ret.extend_from_slice(&dir.part2(need));
-        }
-
-        ret
-    }
-
-    fn find_dir(&mut self, name: &str) -> &mut Dir {
-        if !self.dirs.contains_key(name) {
-            let child_dir = Dir::new(name);
-            self.dirs.insert(name.into(), child_dir);
-        }
-        self.dirs.get_mut(name).unwrap()
-    }
-
-    fn parse(&mut self, lines: &Vec<String>, start: usize) -> usize {
-        let mut pos = start;
-        while pos < lines.len() {
-            let line = &lines[pos];
-
-            if line.is_empty() {
-                continue;
-            }
-
-            let parts: Vec<&str> = line.split(' ').collect();
-            match parts[0] {
-                "$" => match parts[1] {
-                    "ls" => {
-                        pos += 1;
-                    }
-                    "cd" => match parts[2] {
-                        ".." => return pos + 1,
-                        _ => {
-                            let child_dir = self.find_dir(parts[2]);
-                            pos = child_dir.parse(lines, pos + 1);
-                        }
-                    },
-                    _ => unreachable!(),
-                },
-                "dir" => {
-                    // Dir
-                    let _child_dir = self.find_dir(parts[1]);
-                    pos += 1;
-                }
-                _ => {
-                    // File
-                    self.files
-                        .insert(parts[1].into(), parts[0].parse().unwrap());
-                    pos += 1;
+            let mut found = false;
+            for entry in &mut self.entries {
+                if entry.0 == name {
+                    entry.2 += size;
+                    found = true;
+                    break;
                 }
             }
-        }
 
-        lines.len()
+            if !found {
+                let file = i == parts.len() - 1;
+                self.entries.push((name, file, size));
+            }
+        }
+    }
+}
+
+fn build_filename(parent: &str, child: &str) -> String {
+    if parent == "/" {
+        format!("/{child}")
+    } else {
+        format!("{parent}/{child}")
     }
 }
 
@@ -129,16 +56,46 @@ fn main() {
     };
 
     let lines: Vec<String> = lines.flatten().collect();
-    let mut dir = Dir::new("/");
-    dir.parse(&lines, 1);
 
-    println!("{:#?}", dir);
+    let mut files = Files::new();
+    let mut cur_dir = "".to_string();
+    let mut parents = Vec::new();
+    for line in &lines {
+        if line == "$ cd /" {
+            cur_dir = "/".to_string();
+            parents.clear();
+        } else if line == "$ cd .." {
+            cur_dir = parents.pop().unwrap();
+        } else if let Some(line) = line.strip_prefix("$ cd ") {
+            parents.push(cur_dir.clone());
+            cur_dir = build_filename(&cur_dir, line);
+        } else if !line.starts_with("dir ") && line != "$ ls" {
+            let (size, name) = line.split_once(' ').unwrap();
+            let size: usize = size.parse().unwrap();
 
-    let total_disk = 70000000;
-    let space_needed = total_disk - dir.size();
+            files.add(&build_filename(&cur_dir, name), size);
+        }
+    }
 
-    let mut possible = dir.part2(space_needed);
-    possible.sort();
-    println!("needed: {}", space_needed);
-    println!("{:#?}", possible);
+    files.entries.sort();
+
+    let disk_size = 70000000;
+    let space_needed = 30000000;
+    let disk_free = disk_size - files.entries[0].2;
+    let delete_needed = space_needed - disk_free;
+
+    println!("disk_size:{disk_size}");
+    println!("space_needed:{space_needed}");
+    println!("disk_free:{disk_free}");
+    println!("delete_needed:{delete_needed}");
+
+    let mut ans = usize::MAX;
+    for entry in &files.entries {
+        println!("{entry:?}");
+        if !entry.1 && entry.2 >= delete_needed && entry.2 < ans {
+            ans = entry.2;
+        }
+    }
+
+    println!("ans: {ans}");
 }
