2bdea49cc835907f98101fd9b47e5049467cd598
Michael Conrad <mmitton@gmail.com>
2022-12-20 11:07:22 -0500

    2022 - Day 19 - Marginal

--- 2022/day-19/day-19-part-2/src/main.rs
+++ 2022/day-19/day-19-part-2/src/main.rs
@@ -9,73 +9,91 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn run_blueprint(
-    blueprint: &[[u8; 4]; 4],
-    robots: [u8; 4],
-    materials: [u8; 4],
-    minutes: u8,
-) -> [u8; 4] {
-    let mut best: [u8; 4] = materials;
-    let mut best_geode = 0;
+    blueprint: &[[u16; 4]; 4],
+    robots: [u16; 4],
+    materials: [u16; 4],
+    minutes: u16,
+) -> [u16; 4] {
+    let mut best: [u16; 4] = materials;
 
     let mut work = BTreeSet::new();
     let job = (1, materials, robots);
     work.insert(job);
 
-    while let Some((minute, materials, robots)) = work.pop_first() {
+    let mut best_geode = vec![0u16; minutes as usize + 1];
+
+    let mut need_to_build = Vec::new();
+    'build_loop: while let Some((minute, materials, robots)) = work.pop_first() {
+        // println!("minute:{minute}  materials:{materials:?}  robots:{robots:?}");
         let mut new_materials = materials;
-        for (new_material, robot) in new_materials.iter_mut().zip(robots.iter()) {
-            *new_material += robot;
-        }
-        if new_materials[0] > best_geode {
-            best_geode = new_materials[0];
-            println!("new best_geode: {best_geode}");
-            work.retain(|(_minute, materials, robots)| materials[0] + robots[0] + 1 >= best_geode);
-        } else if new_materials[0] + robots[0] + 1 < best_geode {
-            continue;
-        }
-        if minute == minutes {
-            if new_materials[0] > best[0] {
-                println!("new best: {new_materials:?} {robots:?}");
-                best = new_materials;
+        let mut built = [false; 4];
+        for minute in minute..=minutes {
+            if built == [true; 4] {
+                break;
             }
-            continue;
-        }
-
-        // println!("next work: minute:{minute} materials:{materials:?} robots:{robots:?}");
-        for robot in 0..4 {
-            // Can we build a robot now?
-            let mut can_build = true;
-            for (material, needed) in materials.iter().zip(blueprint[robot].iter()) {
-                if material < needed {
-                    can_build = false;
-                    break;
-                }
+            let materials = new_materials;
+            for (new_material, robot) in new_materials.iter_mut().zip(robots.iter()) {
+                *new_material += robot;
+            }
+            // println!("minute:{minute}  materials:{materials:?}  robots:{robots:?}");
+            if new_materials[0] + 2 < best_geode[minute as usize] {
+                continue;
             }
 
-            if can_build {
-                let mut new_materials = new_materials;
-                for (material, needed) in new_materials.iter_mut().zip(blueprint[robot].iter()) {
-                    *material -= needed;
+            if minute == minutes {
+                if new_materials[0] > best[0] {
+                    println!("new best: {new_materials:?} {robots:?}");
+                    best = new_materials;
                 }
+                continue 'build_loop;
+            }
 
-                let mut robots = robots;
-                robots[robot] += 1;
+            need_to_build.clear();
+            need_to_build.push(0);
 
-                // println!("building robot {robot} in minute {minute}  {materials:?} {robots:?}");
-                if new_materials[0] + 2 >= best_geode {
-                    let new_job = (minute + 1, new_materials, robots);
-                    if !work.contains(&new_job) {
-                        work.insert(new_job);
+            for build in 0..4 {
+                if built[build] {
+                    continue;
+                }
+                let mut can_build = true;
+                for (material, needed) in blueprint[build].iter().enumerate().rev() {
+                    if *needed > materials[material] {
+                        can_build = false;
+                        if *needed > materials[material] + robots[material]
+                            && !need_to_build.contains(&material)
+                        {
+                            // println!("need a {material} to build a {build}");
+                            need_to_build.push(material);
+                        }
                     }
                 }
-            }
-        }
 
-        // println!("building robot {robot} in minute {minute}  {materials:?} {robots:?}");
-        if new_materials[0] + 2 >= best_geode {
-            let new_job = (minute + 1, new_materials, robots);
-            if !work.contains(&new_job) {
-                work.insert(new_job);
+                if can_build {
+                    // Build a `build` this minute
+                    let mut new_materials = new_materials;
+                    for (material, needed) in new_materials.iter_mut().zip(blueprint[build].iter())
+                    {
+                        *material -= needed;
+                    }
+
+                    let mut robots = robots;
+                    robots[build] += 1;
+
+                    if best_geode[minute as usize] < materials[0] {
+                        println!("new best @ {minute} {materials:?}");
+                        best_geode[minute as usize] = materials[0];
+                        work.retain(|(_minute, _materials, _robots)| {
+                            _materials[0] + 2 >= best_geode[*_minute as usize]
+                        });
+                    }
+
+                    let new_job = (minute + 1, new_materials, robots);
+                    if !work.contains(&new_job) {
+                        built[build] = true;
+                        work.insert(new_job);
+                        continue;
+                    }
+                }
             }
         }
     }
@@ -103,7 +121,7 @@ fn main() {
             let line = line.replace(':', ".");
             let groups: Vec<&str> = line.split(". ").collect();
 
-            let mut blueprint: [[u8; 4]; 4] = [[0; 4]; 4];
+            let mut blueprint: [[u16; 4]; 4] = [[0; 4]; 4];
 
             for group in &groups[1..] {
                 let group = group.replace(" and ", " ");
