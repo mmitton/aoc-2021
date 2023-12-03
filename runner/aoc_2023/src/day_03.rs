2e13d59b99458644f04794b6bc115aeac6ebbe9d
Michael Conrad <mmitton@gmail.com>
2023-12-03 09:59:11 -0500

    2023 - Day 03

--- runner/aoc_2023/src/day_03.rs
+++ runner/aoc_2023/src/day_03.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, Lines, LinesOpt, Output, RunOutput, Runner};
+use std::collections::{BTreeMap, BTreeSet};
 
 #[derive(Debug)]
 pub enum RunnerError {}
@@ -10,25 +11,121 @@ impl From<RunnerError> for Error {
     }
 }
 
-pub struct Day03 {}
+pub struct Day03 {
+    map: BTreeMap<(isize, isize), char>,
+}
 
 impl Day03 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            map: BTreeMap::new(),
+        }
+    }
+
+    pub fn digit(&self, x: isize, y: isize) -> Option<usize> {
+        if let Some(ch) = self.map.get(&(x, y)) {
+            if ch.is_ascii_digit() {
+                return Some(*ch as usize - '0' as usize);
+            }
+        }
+
+        None
     }
 }
 
 impl Runner for Day03 {
     fn parse(&mut self, path: &str) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        for (y, line) in Lines::from_path(path, LinesOpt::RAW)?.iter().enumerate() {
+            for (x, ch) in line.chars().enumerate() {
+                self.map.insert((x as isize, y as isize), ch);
+            }
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut total = 0;
+        let mut seen = BTreeSet::new();
+        for ((sx, sy), ch) in self.map.iter() {
+            if *ch != '.' && !ch.is_ascii_digit() {
+                println!("found symbol '{ch}' at {sx},{sy}");
+                for dx in -1..=1 {
+                    for dy in -1..=1 {
+                        let x = sx + dx;
+                        let y = sy + dy;
+                        if seen.contains(&(x, y)) {
+                            continue;
+                        }
+                        seen.insert((x, y));
+                        if let Some(mut num) = self.digit(x, y) {
+                            // scan left
+                            let mut lx = x;
+                            while let Some(digit) = self.digit(lx - 1, y) {
+                                lx -= 1;
+                                seen.insert((lx, y));
+                                num = digit;
+                            }
+                            // scan right
+                            let mut rx = lx;
+                            while let Some(digit) = self.digit(rx + 1, y) {
+                                num = num * 10 + digit;
+                                rx += 1;
+                                seen.insert((rx, y));
+                            }
+
+                            println!("found {num} at {lx},{y}");
+                            total += num;
+                        }
+                    }
+                }
+            }
+        }
+        Ok(total.into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut total = 0;
+        for ((sx, sy), ch) in self.map.iter() {
+            if *ch == '*' {
+                let mut seen = BTreeSet::new();
+                let mut nums = Vec::new();
+                println!("found symbol '{ch}' at {sx},{sy}");
+                for dx in -1..=1 {
+                    for dy in -1..=1 {
+                        let x = sx + dx;
+                        let y = sy + dy;
+                        if seen.contains(&(x, y)) {
+                            continue;
+                        }
+                        seen.insert((x, y));
+                        if let Some(mut num) = self.digit(x, y) {
+                            // scan left
+                            let mut lx = x;
+                            while let Some(digit) = self.digit(lx - 1, y) {
+                                lx -= 1;
+                                seen.insert((lx, y));
+                                num = digit;
+                            }
+                            // scan right
+                            let mut rx = lx;
+                            while let Some(digit) = self.digit(rx + 1, y) {
+                                num = num * 10 + digit;
+                                rx += 1;
+                                seen.insert((rx, y));
+                            }
+
+                            println!("found {num} at {lx},{y}");
+                            nums.push(num);
+                        }
+                    }
+                }
+                if nums.len() == 2 {
+                    let ratio = nums[0] * nums[1];
+                    println!("Found a gear as {sx},{sy}  {nums:?}  ratio:{ratio}");
+                    total += ratio;
+                }
+            }
+        }
+        Ok(total.into())
     }
 }
