4deec01f4289ed28b79032ac82d826abf3ffb203
Michael Conrad <mmitton@gmail.com>
2023-12-09 06:59:58 -0500

    2023 - Day 9

--- runner/aoc_2023/src/day_09.rs
+++ runner/aoc_2023/src/day_09.rs
@@ -10,25 +10,88 @@ impl From<RunnerError> for Error {
     }
 }
 
-pub struct Day09 {}
+pub struct Day09 {
+    values: Vec<Vec<isize>>,
+}
 
 impl Day09 {
     pub fn new() -> Self {
-        Self {}
+        Self { values: Vec::new() }
     }
 }
 
 impl Runner for Day09 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        for line in Lines::from_path(path, LinesOpt::RAW)?.iter() {
+            self.values.push(
+                line.split_whitespace()
+                    .map(|v| v.parse().unwrap())
+                    .collect(),
+            );
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut ans = 0;
+
+        for values in self.values.iter() {
+            ans += values.iter().last().unwrap();
+            let mut deltas = vec![values.clone()];
+            loop {
+                let i = deltas.len() - 1;
+                let mut new_row = Vec::new();
+                let mut all_zero = true;
+                for j in 0..deltas[i].len() - 1 {
+                    let v = deltas[i][j + 1] - deltas[i][j];
+                    if v != 0 {
+                        all_zero = false;
+                    }
+                    new_row.push(v);
+                }
+
+                ans += new_row.iter().last().unwrap();
+                deltas.push(new_row);
+                if all_zero {
+                    break;
+                }
+            }
+
+            println!("{deltas:?}");
+        }
+        Ok(ans.into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut ans = 0;
+
+        for values in self.values.iter() {
+            let mut deltas = vec![values.clone()];
+            loop {
+                let i = deltas.len() - 1;
+                let mut new_row = Vec::new();
+                let mut all_zero = true;
+                for j in 0..deltas[i].len() - 1 {
+                    let v = deltas[i][j + 1] - deltas[i][j];
+                    if v != 0 {
+                        all_zero = false;
+                    }
+                    new_row.push(v);
+                }
+
+                deltas.push(new_row);
+                if all_zero {
+                    break;
+                }
+            }
+
+            let mut pred = 0;
+            for values in deltas.iter().rev() {
+                pred = values[0] - pred;
+            }
+            println!("{values:?} => {pred}");
+            ans += pred;
+        }
+        Ok(ans.into())
     }
 }
