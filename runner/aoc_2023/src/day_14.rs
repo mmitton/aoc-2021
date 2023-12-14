a6a6c8a21f02b776214ffefebc38628755450a5c
Michael Conrad <mmitton@gmail.com>
2023-12-14 07:14:40 -0500

    2023 - Day 14 - Add comments

--- runner/aoc_2023/src/day_14.rs
+++ runner/aoc_2023/src/day_14.rs
@@ -13,12 +13,12 @@ impl From<RunnerError> for Error {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Point {
-    x: isize,
-    y: isize,
+    x: i8,
+    y: i8,
 }
 
 impl Point {
-    fn new(x: isize, y: isize) -> Self {
+    fn new(x: i8, y: i8) -> Self {
         Self { x, y }
     }
 }
@@ -62,7 +62,7 @@ impl Dish {
         println!();
     }
 
-    fn add_rock(&mut self, x: isize, y: isize, rounded: bool) {
+    fn add_rock(&mut self, x: i8, y: i8, rounded: bool) {
         self.rocks.push(Rock {
             point: Point::new(x, y),
             rounded,
@@ -73,32 +73,46 @@ impl Dish {
         let mut total_load = 0;
         for rock in self.rocks.iter() {
             if rock.rounded {
-                total_load += self.max.y - rock.point.y + 1;
+                total_load += self.max.y as isize - rock.point.y as isize + 1;
             }
         }
         total_load
     }
 
     fn tilt(&mut self, dir: Dir) {
+        // Tilt the dish in dir Direction.  First sort the rocks so we process all of the rocks in
+        // order by distance from the edge we roll towards.  This lets us keep a cache of the next
+        // open position for all of the rocks rolling in that direction, updating the list to the
+        // rock we just processed and adding one in the opposite direction of the tilt.
+        //
+        // Rolling to the north, the list is all columns, starting at 0, updating to rock.point.y +
+        // 1 so the next rock will tuck in right behind us.  Of course, only updating the position
+        // of rolling rocks.  Cube rocks just update the cached next position to + 1 of the cubed
+        // rock position.
         let (mut stop_at, dx, dy) = match dir {
             Dir::North => {
+                // Sort north to south, open positions start in the north
                 self.rocks.sort_by_key(|r| r.point.y);
                 (vec![0; self.max.x as usize + 1], 0, 1)
             }
             Dir::South => {
+                // Sort south to north, open positions start in the south
                 self.rocks.sort_by_key(|r| self.max.y - r.point.y);
                 (vec![self.max.y; self.max.x as usize + 1], 0, -1)
             }
             Dir::West => {
+                // Sort west to east, open positions start in the west
                 self.rocks.sort_by_key(|r| r.point.x);
                 (vec![0; self.max.y as usize + 1], 1, 0)
             }
             Dir::East => {
+                // Sort east to west, open positions start in the east
                 self.rocks.sort_by_key(|r| self.max.x - r.point.x);
                 (vec![self.max.x; self.max.y as usize + 1], -1, 0)
             }
         };
 
+        // Process all of the rocks now that they are sorted (described above)
         for rock in self.rocks.iter_mut() {
             match dir {
                 Dir::North | Dir::South => {
@@ -124,16 +138,10 @@ impl Dish {
         self.tilt(Dir::East);
     }
 
-    fn rounded_vec(&self) -> Vec<u16> {
+    fn rounded_rocks(&self) -> Vec<Point> {
         self.rocks
             .iter()
-            .filter_map(|r| {
-                if r.rounded {
-                    Some(((r.point.y as u16) << 8) | (r.point.x as u16))
-                } else {
-                    None
-                }
-            })
+            .filter_map(|r| if r.rounded { Some(r.point) } else { None })
             .collect()
     }
 }
@@ -151,10 +159,10 @@ impl Day14 {
 impl Runner for Day14 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
         for (y, line) in Lines::from_path(path, LinesOpt::RAW)?.iter().enumerate() {
-            let y = y as isize;
+            let y = y as i8;
             self.dish.max.y = self.dish.max.y.max(y);
             for (x, c) in line.chars().enumerate() {
-                let x = x as isize;
+                let x = x as i8;
                 self.dish.max.x = self.dish.max.x.max(x);
                 match c {
                     '.' => {}
@@ -176,21 +184,23 @@ impl Runner for Day14 {
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
+        const TOTAL_CYCLES: usize = 1000000000;
+
         self.dish._dump();
+
         let mut seen = HashMap::new();
-        let total_cycles = 1000000000;
-        for i in 1..=total_cycles {
+        for i in 1..=TOTAL_CYCLES {
             self.dish.spin();
             println!("after {i} cycles");
             self.dish._dump();
 
-            let rounded = self.dish.rounded_vec();
+            let rounded = self.dish.rounded_rocks();
             if let Some(cycle_start) = seen.insert(rounded, i) {
                 let cycle_len = i - cycle_start;
-                let remaining = total_cycles - i;
-                let mod_cycles = remaining % cycle_len;
-                println!("Found a cycle from {cycle_start} to {i}, {remaining} cycles, {mod_cycles} mod_cycles");
-                for _ in 0..mod_cycles {
+                let spins_remaining = TOTAL_CYCLES - i;
+                let spins_after_cycles = spins_remaining % cycle_len;
+                println!("Found a cycle from {cycle_start} to {i}, {spins_remaining} spins remaining, {spins_after_cycles} spins after the cycles complete");
+                for _ in 0..spins_after_cycles {
                     self.dish.spin();
                 }
 
