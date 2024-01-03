435c52bfe09c0494019c5e56fedb5e1278e68fd1
Michael Conrad <mmitton@gmail.com>
2024-01-03 05:20:33 -0500

    2019 - Day 4 - Remove list of passwords, just get count

--- runner/aoc_2019/src/day_04.rs
+++ runner/aoc_2019/src/day_04.rs
@@ -12,26 +12,26 @@ impl Day04 {
         Self { range: 0..=0 }
     }
 
-    fn passwords(&self, dup_range: RangeInclusive<u32>) -> Vec<u32> {
-        let mut passwords = Vec::new();
-
+    fn passwords(&self, dup_range: RangeInclusive<u32>) -> u32 {
         fn recurse(
-            passwords: &mut Vec<u32>,
             num: u32,
             pos: u32,
             min: u32,
             dup_range: &RangeInclusive<u32>,
             num_dups: u32,
-        ) {
+            range: &RangeInclusive<u32>,
+        ) -> u32 {
             if pos == 6 {
-                if num_dups > 0 {
-                    passwords.push(num);
+                if num_dups > 0 && range.contains(&num) {
+                    return 1;
                 }
-                return;
+                return 0;
             }
             if min > 9 {
-                return;
+                return 0;
             }
+
+            let mut passwords = 0;
 
             for n in min..=9 {
                 let mut c = n;
@@ -40,24 +40,23 @@ impl Day04 {
                     base *= 10;
                 }
                 for dup in 1..=6 - pos {
-                    recurse(
-                        passwords,
+                    passwords += recurse(
                         num + c * base,
                         pos + dup,
                         n + 1,
                         dup_range,
                         num_dups + if dup_range.contains(&dup) { 1 } else { 0 },
+                        range,
                     );
                     c = (c * 10) + n;
                     base /= 10;
                 }
             }
+
+            passwords
         }
 
-        recurse(&mut passwords, 0, 0, 1, &dup_range, 0);
-
-        passwords.retain(|v| self.range.contains(v));
-        passwords
+        recurse(0, 0, 1, &dup_range, 0, &self.range)
     }
 }
 
@@ -72,10 +71,10 @@ impl Runner for Day04 {
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Ok(self.passwords(2..=6).len().into())
+        Ok(self.passwords(2..=6).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Ok(self.passwords(2..=2).len().into())
+        Ok(self.passwords(2..=2).into())
     }
 }
