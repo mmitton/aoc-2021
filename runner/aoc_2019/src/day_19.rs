c90c747b169e931fde0e4cadf0c9ed7b22610ae6
Michael Conrad <mmitton@gmail.com>
2024-01-11 14:23:57 -0500

    2019 - Day 19

--- runner/aoc_2019/src/day_19.rs
+++ runner/aoc_2019/src/day_19.rs
@@ -1,25 +1,90 @@
+use crate::intcode::{IntCode, State};
 #[allow(unused_imports)]
 use helper::{print, println, Error, Lines, LinesOpt, Output, RunOutput, Runner};
+use std::ops::Range;
 
-pub struct Day19 {}
+pub struct Day19 {
+    intcode: IntCode<isize>,
+    rows: Vec<Range<isize>>,
+}
 
 impl Day19 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            intcode: IntCode::default(),
+            rows: Vec::new(),
+        }
+    }
+
+    fn scan_sector(&mut self, x: isize, y: isize) -> bool {
+        let mut intcode = self.intcode.clone();
+        intcode.input.push_back(x);
+        intcode.input.push_back(y);
+        match intcode.run() {
+            State::HasOutput(v) => v == 1,
+            x => unreachable!("Unexpected state: {x:?}"),
+        }
+    }
+
+    fn run_scan(&mut self, is_done: impl Fn(&[Range<isize>]) -> bool) {
+        let mut last_left = 0;
+        let mut left = 0;
+        let mut right = 0;
+        const MAX_DIFF: isize = 10;
+        for y in 0.. {
+            while !self.scan_sector(left, y) && left - last_left < MAX_DIFF {
+                left += 1;
+            }
+            if left - last_left == MAX_DIFF {
+                left = last_left;
+                self.rows.push(0..0);
+                continue;
+            }
+            if left >= right {
+                right = left + 1;
+            }
+            while self.scan_sector(right, y) {
+                right += 1;
+            }
+            self.rows.push(left..right);
+            if is_done(&self.rows) {
+                break;
+            }
+            last_left = left;
+        }
     }
 }
 
 impl Runner for Day19 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
-        Ok(())
+        self.intcode.load(Lines::from_path(path, LinesOpt::RAW)?)
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        const MAX: isize = 50;
+        self.run_scan(|rows| rows.len() == MAX as usize);
+        Ok(self
+            .rows
+            .iter()
+            .map(|r| r.end.min(MAX - 1) - r.start.min(MAX - 1))
+            .sum::<isize>()
+            .into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        const SQUARE: isize = 100;
+        self.run_scan(|rows| {
+            if rows.len() < SQUARE as usize {
+                false
+            } else {
+                let last_row = &rows[rows.len() - 1];
+                let top_row = &rows[rows.len() - SQUARE as usize];
+                last_row.end - last_row.start >= SQUARE && top_row.end - last_row.start >= SQUARE
+            }
+        });
+        let last_row = &self.rows[self.rows.len() - 1];
+        let x = last_row.start;
+        let y = self.rows.len() as isize - SQUARE;
+        Ok((x * 10_000 + y).into())
     }
 }
