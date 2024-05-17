77e3b393f0d6c067f76b723fc1011a67a1388de9
Michael Conrad <mmitton@gmail.com>
2024-05-17 06:40:44 -0400

    2019 22

--- runner/aoc_2019/src/day_22.rs
+++ runner/aoc_2019/src/day_22.rs
@@ -1,25 +1,88 @@
 #[allow(unused_imports)]
-use helper::{print, println, Error, Lines, LinesOpt, Output, RunOutput, Runner};
+use helper::{
+    modexp, modinverse, print, println, Error, Lines, LinesOpt, Output, RunOutput, Runner,
+};
 
-pub struct Day22 {}
+pub struct Day22 {
+    inst: Vec<Shuffle>,
+}
 
 impl Day22 {
     pub fn new() -> Self {
-        Self {}
+        Self { inst: Vec::new() }
+    }
+}
+
+type Int = i128;
+
+enum Shuffle {
+    NewStack,
+    Increment(Int),
+    Cut(Int),
+}
+
+impl Day22 {
+    fn shuffle(&self, total: Int, card: Int) -> Int {
+        self.inst.iter().fold(card, |card, inst| match inst {
+            Shuffle::NewStack => total - card - 1,
+            Shuffle::Increment(i) => (card * i) % total,
+            Shuffle::Cut(c) => {
+                let c = if *c < 0 { c.abs() } else { total - c.abs() };
+                (card + c) % total
+            }
+        })
+    }
+
+    fn big_shuffle(&self, total: Int, card: Int, times: Int) -> Int {
+        // Convert the whole process to a linear equation: ax + b
+        let (increment_mul, offset_diff) =
+            self.inst
+                .iter()
+                .fold((1, 0), |(increment_mul, offset_diff), inst| {
+                    let (a_new, b_new) = match inst {
+                        Shuffle::NewStack => (-increment_mul, offset_diff - increment_mul),
+                        Shuffle::Cut(n) => (increment_mul, offset_diff + increment_mul * n),
+                        Shuffle::Increment(n) => {
+                            let n = modexp(*n, total - 2, total);
+                            (increment_mul * n, offset_diff)
+                        }
+                    };
+                    (a_new.rem_euclid(total), b_new.rem_euclid(total))
+                });
+        let increment = modexp(increment_mul, times, total);
+        let offset = (offset_diff * (1_i128 - increment)).rem_euclid(total)
+            * modexp(1 - increment_mul, total - 2, total);
+
+        (offset + increment * card).rem_euclid(total)
     }
 }
 
 impl Runner for Day22 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::RAW)?;
+        for line in lines.iter() {
+            let mut parts: Vec<&str> = line.split_whitespace().collect();
+            let num = parts.pop().unwrap();
+            match parts.pop().unwrap() {
+                "new" => self.inst.push(Shuffle::NewStack),
+                "increment" => self.inst.push(Shuffle::Increment(num.parse()?)),
+                "cut" => self.inst.push(Shuffle::Cut(num.parse()?)),
+                _ => unreachable!(),
+            }
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.shuffle(10007, 2019).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self
+            .big_shuffle(119315717514047, 2020, 101741582076661)
+            .into())
+        // Ok(self
+        //     .big_shuffle(119315717514047, 2020, 101741582076661)
+        //     .into())
     }
 }
--- runner/helper/src/run_output.rs
+++ runner/helper/src/run_output.rs
@@ -4,6 +4,8 @@ pub enum RunOutput {
     String(String),
     Usize(usize),
     Isize(isize),
+    U128(u128),
+    I128(i128),
 }
 
 impl Display for RunOutput {
@@ -12,6 +14,8 @@ impl Display for RunOutput {
             Self::String(s) => write!(f, "{s}"),
             Self::Usize(v) => write!(f, "{v}"),
             Self::Isize(v) => write!(f, "{v}"),
+            Self::U128(v) => write!(f, "{v}"),
+            Self::I128(v) => write!(f, "{v}"),
         }
     }
 }
@@ -31,9 +35,11 @@ impl_from!(u8, usize, Usize);
 impl_from!(u16, usize, Usize);
 impl_from!(u32, usize, Usize);
 impl_from!(u64, usize, Usize);
+impl_from!(u128, u128, U128);
 impl_from!(usize, usize, Usize);
 impl_from!(i8, isize, Isize);
 impl_from!(i16, isize, Isize);
 impl_from!(i32, isize, Isize);
 impl_from!(i64, isize, Isize);
 impl_from!(isize, isize, Isize);
+impl_from!(i128, i128, I128);
