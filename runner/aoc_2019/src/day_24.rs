f2a97ccdbb1cea2dff5c26c47f2f52518ae62a0f
Michael Conrad <mmitton@gmail.com>
2024-05-17 09:02:56 -0400

    2019 24 - Optimize out hashset, using bit masks now

--- runner/aoc_2019/src/day_24.rs
+++ runner/aoc_2019/src/day_24.rs
@@ -1,100 +1,45 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, Lines, LinesOpt, Output, RunOutput, Runner};
-use std::collections::HashSet;
 
-#[derive(Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash)]
-struct Tile {
-    x: i8,
-    y: i8,
-    z: i8,
-}
-
-impl std::fmt::Debug for Tile {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({},{},{})", self.x, self.y, self.z)
-    }
-}
-
-impl Tile {
-    fn neighbor_count(
-        &self,
-        tiles: &HashSet<Tile>,
-        recurse: bool,
-        neighbors: &[Vec<Vec<Tile>>],
-    ) -> u8 {
-        if !recurse {
-            [(-1, 0), (1, 0), (0, -1), (0, 1)]
-                .iter()
-                .map(|(dx, dy)| {
-                    if tiles.contains(&Tile {
-                        x: self.x + dx,
-                        y: self.y + dy,
-                        z: self.z,
-                    }) {
-                        1
-                    } else {
-                        0
-                    }
-                })
-                .sum()
-        } else {
-            neighbors[self.y as usize][self.x as usize]
-                .iter()
-                .map(|neighbor| {
-                    if tiles.contains(&Tile {
-                        x: neighbor.x,
-                        y: neighbor.y,
-                        z: self.z + neighbor.z,
-                    }) {
-                        1
-                    } else {
-                        0
-                    }
-                })
-                .sum()
-        }
-    }
+macro_rules! tile_bit {
+    ($x:expr, $y:expr) => {
+        (1 << (($y) * 5) + ($x))
+    };
 }
 
 pub struct Day24 {
-    tiles: HashSet<Tile>,
-    neighbors: Vec<Vec<Vec<Tile>>>,
-    z_min: i8,
-    z_max: i8,
+    tiles: [u32; 256],
+    neighbors: Vec<Vec<Vec<(u32, u8)>>>,
+    z_min: u8,
+    z_max: u8,
 }
 
 impl Day24 {
+    const LEVEL_0: u8 = 128;
     pub fn new() -> Self {
         Self {
-            tiles: HashSet::new(),
+            tiles: [0; 256],
             neighbors: Vec::new(),
-            z_min: 0,
-            z_max: 0,
+            z_min: Self::LEVEL_0,
+            z_max: Self::LEVEL_0,
         }
     }
 
-    fn _print(&self, recurse: bool) {
-        for z in self.z_min..=self.z_max {
-            println!();
-            println!("Depth {z}");
-            for y in 0..5 {
-                for x in 0..5 {
-                    if x == 2 && y == 2 && recurse {
-                        assert!(!self.tiles.contains(&Tile { x, y, z }));
-                        print!("?");
-                    } else if self.tiles.contains(&Tile { x, y, z }) {
-                        print!("#");
-                    } else {
-                        print!(".");
-                    }
+    fn neighbor_count(&self, x: u8, y: u8, z: u8) -> u8 {
+        self.neighbors[y as usize][x as usize]
+            .iter()
+            .map(|(bit, dz)| {
+                if self.tiles[z.wrapping_add(*dz) as usize] & bit != 0 {
+                    1
+                } else {
+                    0
                 }
-                println!();
-            }
-        }
+            })
+            .sum()
     }
 
     fn tick(&mut self, recurse: bool) {
-        let mut next_tiles = HashSet::new();
+        let mut next_tiles = [0; 256];
 
         for z in self.z_min - 1..=self.z_max + 1 {
             for y in 0..5 {
@@ -102,24 +47,16 @@ impl Day24 {
                     if recurse && y == 2 && x == 2 {
                         continue;
                     }
-                    let tile = Tile { x, y, z };
-                    let neighbor_count = tile.neighbor_count(&self.tiles, recurse, &self.neighbors);
-                    // if z == 1 && x == 4 && self.neighbors.len() > 0 {
-                    //     println!(
-                    //         "Tile {tile:?} is currently alive? {}  Has {} neighbors  {:?}",
-                    //         self.tiles.contains(&tile),
-                    //         neighbor_count,
-                    //         self.neighbors[y as usize][x as usize]
-                    //     );
-                    // }
-                    if self.tiles.contains(&tile) {
+                    let neighbor_count = self.neighbor_count(x, y, z);
+                    let bit = tile_bit!(x, y);
+                    if self.tiles[z as usize] & bit != 0 {
                         if neighbor_count == 1 {
-                            next_tiles.insert(tile);
+                            next_tiles[z as usize] |= bit;
                             self.z_min = self.z_min.min(z);
                             self.z_max = self.z_max.max(z);
                         }
                     } else if neighbor_count == 1 || neighbor_count == 2 {
-                        next_tiles.insert(tile);
+                        next_tiles[z as usize] |= bit;
                         self.z_min = self.z_min.min(z);
                         self.z_max = self.z_max.max(z);
                     }
@@ -138,13 +75,7 @@ impl Runner for Day24 {
             for (x, c) in line.chars().enumerate() {
                 match c {
                     '.' => {}
-                    '#' => {
-                        let _ = self.tiles.insert(Tile {
-                            x: x as i8,
-                            y: y as i8,
-                            z: 0,
-                        });
-                    }
+                    '#' => self.tiles[Self::LEVEL_0 as usize] |= tile_bit!(x, y),
                     _ => unreachable!(),
                 }
             }
@@ -153,21 +84,43 @@ impl Runner for Day24 {
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        fn tiles_to_u32(tiles: &HashSet<Tile>) -> u32 {
-            tiles
-                .iter()
-                .fold(0, |acc, tile| acc | (1 << ((tile.y * 5) + tile.x)))
-        }
+        for y in 0..5 {
+            let mut row = Vec::new();
+            for x in 0..5 {
+                let mut cell = Vec::new();
 
-        let mut seen = HashSet::new();
-        seen.insert(tiles_to_u32(&self.tiles));
+                // Calc up/down tiles
+                if y == 0 {
+                    cell.push((tile_bit!(x, y + 1), 0));
+                } else if y == 4 {
+                    cell.push((tile_bit!(x, y - 1), 0));
+                } else {
+                    cell.push((tile_bit!(x, y - 1), 0));
+                    cell.push((tile_bit!(x, y + 1), 0));
+                }
+
+                // Calc left/right tiles
+                if x == 0 {
+                    cell.push((tile_bit!(x + 1, y), 0));
+                } else if x == 4 {
+                    cell.push((tile_bit!(x - 1, y), 0));
+                } else {
+                    cell.push((tile_bit!(x - 1, y), 0));
+                    cell.push((tile_bit!(x + 1, y), 0));
+                }
+                row.push(cell);
+            }
+            self.neighbors.push(row);
+        }
+        let mut seen = std::collections::HashSet::new();
+        seen.insert(self.tiles[Self::LEVEL_0 as usize]);
         loop {
             self.tick(false);
-            if !seen.insert(tiles_to_u32(&self.tiles)) {
+            if !seen.insert(self.tiles[Self::LEVEL_0 as usize]) {
                 break;
             }
         }
-        Ok(tiles_to_u32(&self.tiles).into())
+        Ok(self.tiles[Self::LEVEL_0 as usize].into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
@@ -178,54 +131,54 @@ impl Runner for Day24 {
 
                 // Calc up/down tiles
                 if y == 0 {
-                    cell.push(Tile { x: 2, y: 1, z: -1 });
-                    cell.push(Tile { x, y: 1, z: 0 });
+                    cell.push((tile_bit!(2, 1), 255));
+                    cell.push((tile_bit!(x, 1), 0));
                 } else if y == 4 {
-                    cell.push(Tile { x, y: 3, z: 0 });
-                    cell.push(Tile { x: 2, y: 3, z: -1 });
+                    cell.push((tile_bit!(x, 3), 0));
+                    cell.push((tile_bit!(2, 3), 255));
                 } else if y == 1 && x == 2 {
-                    cell.push(Tile { x: 0, y: 0, z: 1 });
-                    cell.push(Tile { x: 1, y: 0, z: 1 });
-                    cell.push(Tile { x: 2, y: 0, z: 1 });
-                    cell.push(Tile { x: 3, y: 0, z: 1 });
-                    cell.push(Tile { x: 4, y: 0, z: 1 });
-                    cell.push(Tile { x, y: y - 1, z: 0 });
+                    cell.push((tile_bit!(0, 0), 1));
+                    cell.push((tile_bit!(1, 0), 1));
+                    cell.push((tile_bit!(2, 0), 1));
+                    cell.push((tile_bit!(3, 0), 1));
+                    cell.push((tile_bit!(4, 0), 1));
+                    cell.push((tile_bit!(x, y - 1), 0));
                 } else if y == 3 && x == 2 {
-                    cell.push(Tile { x: 0, y: 4, z: 1 });
-                    cell.push(Tile { x: 1, y: 4, z: 1 });
-                    cell.push(Tile { x: 2, y: 4, z: 1 });
-                    cell.push(Tile { x: 3, y: 4, z: 1 });
-                    cell.push(Tile { x: 4, y: 4, z: 1 });
-                    cell.push(Tile { x, y: y + 1, z: 0 });
+                    cell.push((tile_bit!(0, 4), 1));
+                    cell.push((tile_bit!(1, 4), 1));
+                    cell.push((tile_bit!(2, 4), 1));
+                    cell.push((tile_bit!(3, 4), 1));
+                    cell.push((tile_bit!(4, 4), 1));
+                    cell.push((tile_bit!(x, y + 1), 0));
                 } else {
-                    cell.push(Tile { x, y: y - 1, z: 0 });
-                    cell.push(Tile { x, y: y + 1, z: 0 });
+                    cell.push((tile_bit!(x, y - 1), 0));
+                    cell.push((tile_bit!(x, y + 1), 0));
                 }
 
                 // Calc left/right tiles
                 if x == 0 {
-                    cell.push(Tile { x: 1, y: 2, z: -1 });
-                    cell.push(Tile { x: 1, y, z: 0 });
+                    cell.push((tile_bit!(1, 2), 255));
+                    cell.push((tile_bit!(1, y), 0));
                 } else if x == 4 {
-                    cell.push(Tile { x: 3, y, z: 0 });
-                    cell.push(Tile { x: 3, y: 2, z: -1 });
+                    cell.push((tile_bit!(3, y), 0));
+                    cell.push((tile_bit!(3, 2), 255));
                 } else if x == 1 && y == 2 {
-                    cell.push(Tile { x: 0, y: 0, z: 1 });
-                    cell.push(Tile { x: 0, y: 1, z: 1 });
-                    cell.push(Tile { x: 0, y: 2, z: 1 });
-                    cell.push(Tile { x: 0, y: 3, z: 1 });
-                    cell.push(Tile { x: 0, y: 4, z: 1 });
-                    cell.push(Tile { x: x - 1, y, z: 0 });
+                    cell.push((tile_bit!(0, 0), 1));
+                    cell.push((tile_bit!(0, 1), 1));
+                    cell.push((tile_bit!(0, 2), 1));
+                    cell.push((tile_bit!(0, 3), 1));
+                    cell.push((tile_bit!(0, 4), 1));
+                    cell.push((tile_bit!(x - 1, y), 0));
                 } else if x == 3 && y == 2 {
-                    cell.push(Tile { x: 4, y: 0, z: 1 });
-                    cell.push(Tile { x: 4, y: 1, z: 1 });
-                    cell.push(Tile { x: 4, y: 2, z: 1 });
-                    cell.push(Tile { x: 4, y: 3, z: 1 });
-                    cell.push(Tile { x: 4, y: 4, z: 1 });
-                    cell.push(Tile { x: x + 1, y, z: 0 });
+                    cell.push((tile_bit!(4, 0), 1));
+                    cell.push((tile_bit!(4, 1), 1));
+                    cell.push((tile_bit!(4, 2), 1));
+                    cell.push((tile_bit!(4, 3), 1));
+                    cell.push((tile_bit!(4, 4), 1));
+                    cell.push((tile_bit!(x + 1, y), 0));
                 } else {
-                    cell.push(Tile { x: x - 1, y, z: 0 });
-                    cell.push(Tile { x: x + 1, y, z: 0 });
+                    cell.push((tile_bit!(x - 1, y), 0));
+                    cell.push((tile_bit!(x + 1, y), 0));
                 }
 
                 row.push(cell);
@@ -237,6 +190,11 @@ impl Runner for Day24 {
             self.tick(true);
         }
 
-        Ok(self.tiles.len().into())
+        Ok(self
+            .tiles
+            .iter()
+            .map(|level| level.count_ones())
+            .sum::<u32>()
+            .into())
     }
 }
