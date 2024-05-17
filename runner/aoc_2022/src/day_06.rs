d785f7d4ba1dd0e3a8d248027feaa57d83a7e35d
Michael Conrad <mmitton@gmail.com>
2024-05-17 11:24:09 -0400

    2022 06

--- runner/aoc_2022/src/day_06.rs
+++ runner/aoc_2022/src/day_06.rs
@@ -1,34 +1,43 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, Lines, LinesOpt, Output, RunOutput, Runner};
 
-#[derive(Debug)]
-pub enum RunnerError {}
-
-impl From<RunnerError> for Error {
-    fn from(e: RunnerError) -> Self {
-        Self::Runner(format!("{e:?}"))
-    }
+pub struct Day06 {
+    chars: Vec<u32>,
 }
-
-pub struct Day06 {}
 
 impl Day06 {
     pub fn new() -> Self {
-        Self {}
+        Self { chars: Vec::new() }
     }
+}
+
+fn find_marker<const N: usize>(chars: &[u32]) -> usize {
+    let mut window: [u32; N] = std::array::from_fn(|i| chars[i]);
+
+    for i in N..chars.len() {
+        window[i % N] = chars[i];
+
+        if window.iter().fold(0, |acc, c| acc | c).count_ones() as usize == N {
+            return i + 1;
+        }
+    }
+
+    0
 }
 
 impl Runner for Day06 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::RAW)?;
+        self.chars
+            .extend(lines[0].chars().map(|c| 1u32 << (c as u8 - b'a')));
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(find_marker::<4>(&self.chars).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(find_marker::<14>(&self.chars).into())
     }
 }
