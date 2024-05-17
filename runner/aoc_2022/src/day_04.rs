0d7e4636f4a1c78455ee22d80312fd71f7912a68
Michael Conrad <mmitton@gmail.com>
2024-05-17 09:31:51 -0400

    2022 04

--- runner/aoc_2022/src/day_04.rs
+++ runner/aoc_2022/src/day_04.rs
@@ -1,34 +1,73 @@
+use std::ops::RangeInclusive;
+
 #[allow(unused_imports)]
 use helper::{print, println, Error, Lines, LinesOpt, Output, RunOutput, Runner};
 
-#[derive(Debug)]
-pub enum RunnerError {}
-
-impl From<RunnerError> for Error {
-    fn from(e: RunnerError) -> Self {
-        Self::Runner(format!("{e:?}"))
-    }
+pub struct Day04 {
+    elves: Vec<(RangeInclusive<usize>, RangeInclusive<usize>)>,
 }
-
-pub struct Day04 {}
 
 impl Day04 {
     pub fn new() -> Self {
-        Self {}
+        Self { elves: Vec::new() }
     }
 }
 
 impl Runner for Day04 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::RAW)?;
+        for line in lines.iter() {
+            let (elf1, elf2) = line.split_once(',').unwrap();
+
+            macro_rules! range {
+                ($range:expr) => {{
+                    let (lo, hi) = $range.split_once('-').unwrap();
+                    let lo: usize = lo.parse().unwrap();
+                    let hi: usize = hi.parse().unwrap();
+                    lo..=hi
+                }};
+            }
+            let elf1 = range!(elf1);
+            let elf2 = range!(elf2);
+            self.elves.push((elf1, elf2));
+        }
+
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self
+            .elves
+            .iter()
+            .map(|(elf1, elf2)| {
+                if elf1.contains(elf2.start()) && elf1.contains(elf2.end())
+                    || elf2.contains(elf1.start()) && elf2.contains(elf1.end())
+                {
+                    1
+                } else {
+                    0
+                }
+            })
+            .sum::<usize>()
+            .into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self
+            .elves
+            .iter()
+            .map(|(elf1, elf2)| {
+                if elf1.contains(elf2.start())
+                    || elf1.contains(elf2.end())
+                    || elf2.contains(elf1.start())
+                    || elf2.contains(elf1.end())
+                {
+                    1
+                } else {
+                    0
+                }
+            })
+            .sum::<usize>()
+            .into())
     }
 }
