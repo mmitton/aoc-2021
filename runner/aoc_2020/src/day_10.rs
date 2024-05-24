3dac5b493d3f6f996c89d6228a2aea314d0990d3
Michael Conrad <mmitton@gmail.com>
2024-05-24 15:39:12 -0400

    2020 10

--- runner/aoc_2020/src/day_10.rs
+++ runner/aoc_2020/src/day_10.rs
@@ -1,25 +1,50 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
-pub struct Day10 {}
+pub struct Day10 {
+    adapters: Vec<usize>,
+}
 
 impl Day10 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            adapters: Vec::new(),
+        }
     }
 }
 
 impl Runner for Day10 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::RAW)?;
+        self.adapters
+            .extend(lines.iter().map(|l| l.parse::<usize>().unwrap()));
+        self.adapters.sort();
+        self.adapters.insert(0, 0);
+        self.adapters
+            .push(self.adapters[self.adapters.len() - 1] + 3);
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut diff = [0; 4];
+
+        for a in self.adapters.windows(2) {
+            diff[a[1] - a[0]] += 1;
+        }
+        Ok((diff[1] * diff[3]).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut totals = vec![0usize; self.adapters.len()];
+        totals[self.adapters.len() - 1] = 1;
+
+        for i in (0..self.adapters.len()).rev() {
+            for j in (0..i).rev() {
+                if self.adapters[i] - self.adapters[j] <= 3 {
+                    totals[j] += totals[i];
+                }
+            }
+        }
+        Ok(totals[0].into())
     }
 }
