8d3f8892006e616c25e6f4fbc1265875f388b821
Michael Conrad <mmitton@gmail.com>
2024-05-24 11:03:58 -0400

    2020 01

--- runner/aoc_2020/src/day_01.rs
+++ runner/aoc_2020/src/day_01.rs
@@ -1,25 +1,45 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
-pub struct Day01 {}
+pub struct Day01 {
+    nums: Vec<usize>,
+}
 
 impl Day01 {
     pub fn new() -> Self {
-        Self {}
+        Self { nums: Vec::new() }
     }
 }
 
 impl Runner for Day01 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::RAW)?;
+        self.nums
+            .extend(lines.iter().map(|l| l.parse::<usize>().unwrap()));
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
+        for (i, a) in self.nums.iter().enumerate() {
+            for b in self.nums[i + 1..].iter() {
+                if a + b == 2020 {
+                    return Ok((a * b).into());
+                }
+            }
+        }
         Err(Error::Unsolved)
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
+        for (i, a) in self.nums.iter().enumerate() {
+            for (j, b) in self.nums[i + 1..].iter().enumerate() {
+                for c in self.nums[j + 1..].iter() {
+                    if a + b + c == 2020 {
+                        return Ok((a * b * c).into());
+                    }
+                }
+            }
+        }
         Err(Error::Unsolved)
     }
 }
