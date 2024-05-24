8b9c5edc4440b937837aae9bc6e5d1fe42d9e69a
Michael Conrad <mmitton@gmail.com>
2024-05-24 11:17:45 -0400

    2020 02

--- runner/aoc_2020/src/day_02.rs
+++ runner/aoc_2020/src/day_02.rs
@@ -1,25 +1,66 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
-pub struct Day02 {}
+pub struct Day02 {
+    policies: Vec<Policy>,
+}
+
+#[derive(Debug)]
+struct Policy {
+    indexes: [usize; 2],
+    chars: Vec<char>,
+    target: char,
+}
 
 impl Day02 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            policies: Vec::new(),
+        }
     }
 }
 
 impl Runner for Day02 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::RAW)?;
+        for line in lines.iter() {
+            let mut parts = line.split_whitespace();
+            let a = parts.next().unwrap();
+            let b = parts.next().unwrap();
+            let c: Vec<char> = parts.next().unwrap().chars().collect();
+
+            let (p1, p2) = a.split_once('-').unwrap();
+            self.policies.push(Policy {
+                indexes: [p1.parse::<usize>()?, p2.parse::<usize>()?],
+                chars: c,
+                target: b.chars().nth(0).unwrap(),
+            });
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self
+            .policies
+            .iter()
+            .filter(|p| {
+                (p.indexes[0]..=p.indexes[1])
+                    .contains(&p.chars.iter().filter(|c| **c == p.target).count())
+            })
+            .count()
+            .into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self
+            .policies
+            .iter()
+            .filter(|p| {
+                let a = p.chars[p.indexes[0] - 1];
+                let b = p.chars[p.indexes[1] - 1];
+                (a == p.target || b == p.target) && a != b
+            })
+            .count()
+            .into())
     }
 }
