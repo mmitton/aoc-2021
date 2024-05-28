88db46e1d881b8e0f1f392e6b8139244572f2b93
Michael Conrad <mmitton@gmail.com>
2024-05-28 13:51:30 -0400

    2020 17

--- runner/aoc_2020/src/day_17.rs
+++ runner/aoc_2020/src/day_17.rs
@@ -2,7 +2,7 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
 pub struct Day17 {
-    initial: Vec<[isize; 2]>,
+    initial: Vec<[i8; 2]>,
 }
 
 impl Day17 {
@@ -12,19 +12,45 @@ impl Day17 {
         }
     }
 
-    pub fn boot<const DIM: usize>(&self) -> usize {
-        type D<const DIM: usize> = [isize; DIM];
-        let mut cur = HashSet::default();
-        let mut next = HashSet::default();
+    pub fn boot(&self, w_delta: i8) -> usize {
+        let mut cur = HashMap::default();
+        let mut next = HashMap::default();
 
-        for initial in self.initial.iter() {
-            let mut c: [isize; DIM] = [0; DIM];
-            c[0] = initial[0];
-            c[1] = initial[1];
-            cur.insert(c);
+        fn set(map: &mut HashMap<[i8; 4], (u8, bool)>, c: [i8; 4], w_delta: i8) {
+            map.entry(c).or_default().1 = true;
+            for w in -w_delta..=w_delta {
+                for z in -1..=1 {
+                    for y in -1..=1 {
+                        for x in -1..=1 {
+                            if x != 0 || y != 0 || z != 0 || w != 0 {
+                                map.entry([c[0] + x, c[1] + y, c[2] + z, c[3] + w])
+                                    .or_default()
+                                    .0 += 1;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        cur.len()
+        for initial in self.initial.iter() {
+            set(&mut cur, [initial[0], initial[1], 0, 0], w_delta);
+        }
+
+        for _ in 0..6 {
+            next.clear();
+            for (c, v) in cur.iter() {
+                if v.1 && (v.0 == 2 || v.0 == 3) {
+                    set(&mut next, *c, w_delta);
+                }
+                if !v.1 && v.0 == 3 {
+                    set(&mut next, *c, w_delta);
+                }
+            }
+            std::mem::swap(&mut cur, &mut next);
+        }
+
+        cur.values().filter(|(_, on)| *on).count()
     }
 }
 
@@ -34,7 +60,7 @@ impl Runner for Day17 {
         for (y, row) in lines.iter().enumerate() {
             for (x, c) in row.chars().enumerate() {
                 if c == '#' {
-                    self.initial.push([x, y])
+                    self.initial.push([x as i8, y as i8]);
                 }
             }
         }
@@ -42,10 +68,10 @@ impl Runner for Day17 {
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.boot(0).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.boot(1).into())
     }
 }
