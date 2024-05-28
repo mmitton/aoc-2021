81a6a308ece36cb35c7880f3d2e8677ba223fd93
Michael Conrad <mmitton@gmail.com>
2024-05-28 12:56:13 -0400

    2020 16

--- runner/aoc_2020/src/day_17.rs
+++ runner/aoc_2020/src/day_17.rs
@@ -1,17 +1,43 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
-pub struct Day17 {}
+pub struct Day17 {
+    initial: Vec<[isize; 2]>,
+}
 
 impl Day17 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            initial: Vec::new(),
+        }
+    }
+
+    pub fn boot<const DIM: usize>(&self) -> usize {
+        type D<const DIM: usize> = [isize; DIM];
+        let mut cur = HashSet::default();
+        let mut next = HashSet::default();
+
+        for initial in self.initial.iter() {
+            let mut c: [isize; DIM] = [0; DIM];
+            c[0] = initial[0];
+            c[1] = initial[1];
+            cur.insert(c);
+        }
+
+        cur.len()
     }
 }
 
 impl Runner for Day17 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::RAW)?;
+        for (y, row) in lines.iter().enumerate() {
+            for (x, c) in row.chars().enumerate() {
+                if c == '#' {
+                    self.initial.push([x, y])
+                }
+            }
+        }
         Ok(())
     }
 
