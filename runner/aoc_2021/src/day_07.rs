213a2d5146b4ed8df3fb9b46f4a0ce0e100ba9a0
Michael Conrad <mmitton@gmail.com>
2024-05-31 14:58:01 -0400

    2021 Day 07

--- runner/aoc_2021/src/day_07.rs
+++ runner/aoc_2021/src/day_07.rs
@@ -1,25 +1,61 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
-pub struct Day07 {}
+pub struct Day07 {
+    crabs: Vec<usize>,
+    costs: Vec<usize>,
+    max: usize,
+}
 
 impl Day07 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            crabs: Vec::new(),
+            costs: Vec::new(),
+            max: 0,
+        }
+    }
+
+    fn make_costs(&mut self, non_const: bool) {
+        let mut next = 1;
+        self.costs.push(0);
+        for step in 1..=self.max {
+            self.costs.push(next);
+            next += if !non_const { 1 } else { step + 1 };
+        }
+    }
+
+    fn calc_min_cost(&self) -> usize {
+        let mut min = usize::MAX;
+        for pos in 1..=self.max {
+            let cost = self
+                .crabs
+                .iter()
+                .map(|c| self.costs[(*c as isize - pos as isize).unsigned_abs()])
+                .sum();
+            min = min.min(cost);
+        }
+
+        min
     }
 }
 
 impl Runner for Day07 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::RAW)?;
+        self.crabs
+            .extend(lines[0].split(',').map(|c| c.parse::<usize>().unwrap()));
+        self.max = *self.crabs.iter().max().unwrap();
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        self.make_costs(false);
+        Ok(self.calc_min_cost().into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        self.make_costs(true);
+        Ok(self.calc_min_cost().into())
     }
 }
