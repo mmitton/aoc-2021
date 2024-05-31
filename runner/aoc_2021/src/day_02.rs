f90f90d61b96d9184096eaf14912b194211dd863
Michael Conrad <mmitton@gmail.com>
2024-05-31 10:25:02 -0400

    2021 Day 02

--- runner/aoc_2021/src/day_02.rs
+++ runner/aoc_2021/src/day_02.rs
@@ -1,25 +1,60 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
-pub struct Day02 {}
+enum Command {
+    Forward(usize),
+    Down(usize),
+    Up(usize),
+}
+
+pub struct Day02 {
+    commands: Vec<Command>,
+}
 
 impl Day02 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            commands: Vec::new(),
+        }
     }
 }
 
 impl Runner for Day02 {
     fn parse(&mut self, path: &str, _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_path(path, LinesOpt::RAW)?;
+        let lines = Lines::from_path(path, LinesOpt::ALL)?;
+        self.commands.extend(lines.iter().map(|l| {
+            let (cmd, num) = l.split_once(' ').unwrap();
+            let num: usize = num.parse().unwrap();
+            match cmd {
+                "forward" => Command::Forward(num),
+                "up" => Command::Up(num),
+                "down" => Command::Down(num),
+                _ => unreachable!(),
+            }
+        }));
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let (mut x, mut y) = (0, 0);
+        self.commands.iter().for_each(|cmd| match cmd {
+            Command::Forward(n) => x += n,
+            Command::Up(n) => y -= n,
+            Command::Down(n) => y += n,
+        });
+        Ok((x * y).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let (mut x, mut y, mut aim) = (0, 0, 0);
+        self.commands.iter().for_each(|cmd| match cmd {
+            Command::Forward(n) => {
+                x += n;
+                y += aim * n;
+            }
+            Command::Up(n) => aim -= n,
+            Command::Down(n) => aim += n,
+        });
+        Ok((x * y).into())
     }
 }
