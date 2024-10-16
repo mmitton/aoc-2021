f6c80b978935cb8b0b68c84fe950f4e706a8c31f
Michael Conrad <mmitton@gmail.com>
2024-10-16 07:51:28 -0400

    2015 Day 1

--- runner/aoc_2015/src/day_01.rs
+++ runner/aoc_2015/src/day_01.rs
@@ -2,7 +2,9 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
 #[derive(Default)]
-pub struct Day01 {}
+pub struct Day01 {
+    moves: Vec<isize>,
+}
 
 impl Day01 {
     pub fn new() -> Self {
@@ -12,15 +14,31 @@ impl Day01 {
 
 impl Runner for Day01 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        assert_eq!(lines.len(), 1);
+        for c in lines[0].chars() {
+            let n = match c {
+                '(' => 1,
+                ')' => -1,
+                _ => return Err(Error::InvalidInput(format!("{c}"))),
+            };
+            self.moves.push(n);
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.moves.iter().sum::<isize>().into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
+        let mut floor = 0;
+        for (i, n) in self.moves.iter().enumerate() {
+            floor += n;
+            if floor == -1 {
+                return Ok((i + 1).into());
+            }
+        }
         Err(Error::Unsolved)
     }
 }
