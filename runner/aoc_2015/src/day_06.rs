67ad39e6ff306889ce24a1c525ae44ad96376ed2
Michael Conrad <mmitton@gmail.com>
2024-10-17 05:22:22 -0400

    2015 Day 6

--- runner/aoc_2015/src/day_06.rs
+++ runner/aoc_2015/src/day_06.rs
@@ -1,8 +1,48 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
+#[derive(Debug)]
+enum OnOff {
+    On,
+    Off,
+    Toggle,
+}
+
+#[derive(Debug)]
+struct Instruction {
+    on_off: OnOff,
+    x1: u32,
+    y1: u32,
+    x2: u32,
+    y2: u32,
+}
+
+struct Grid(Vec<usize>);
+
+impl Grid {
+    fn new() -> Self {
+        Self(vec![0; 1000 * 1000])
+    }
+
+    fn process<F>(&mut self, instructions: &[Instruction], f: F)
+    where
+        F: Fn(&OnOff, usize) -> usize,
+    {
+        for instruction in instructions.iter() {
+            for y in instruction.y1..=instruction.y2 {
+                for x in instruction.x1..=instruction.x2 {
+                    let idx = ((y * 1000) + x) as usize;
+                    self.0[idx] = f(&instruction.on_off, self.0[idx]);
+                }
+            }
+        }
+    }
+}
+
 #[derive(Default)]
-pub struct Day06 {}
+pub struct Day06 {
+    instructions: Vec<Instruction>,
+}
 
 impl Day06 {
     pub fn new() -> Self {
@@ -12,15 +52,48 @@ impl Day06 {
 
 impl Runner for Day06 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::ALL)?;
+        for line in lines.iter() {
+            let (on_off, line) = if line.starts_with("turn on") {
+                (OnOff::On, &line[8..])
+            } else if line.starts_with("turn off") {
+                (OnOff::Off, &line[9..])
+            } else {
+                (OnOff::Toggle, &line[7..])
+            };
+
+            let line = line.replace(" through ", " ");
+            let line = line.replace(",", " ");
+            let parts: Vec<&str> = line.split(" ").collect();
+
+            self.instructions.push(Instruction {
+                on_off,
+                x1: parts[0].parse()?,
+                y1: parts[1].parse()?,
+                x2: parts[2].parse()?,
+                y2: parts[3].parse()?,
+            });
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut grid = Grid::new();
+        grid.process(&self.instructions, |on_off, v| match on_off {
+            OnOff::On => 1,
+            OnOff::Off => 0,
+            OnOff::Toggle => 1 - v,
+        });
+        Ok(grid.0.iter().sum::<usize>().into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut grid = Grid::new();
+        grid.process(&self.instructions, |on_off, v| match on_off {
+            OnOff::On => v + 1,
+            OnOff::Off => v.saturating_sub(1),
+            OnOff::Toggle => v + 2,
+        });
+        Ok(grid.0.iter().sum::<usize>().into())
     }
 }
