c8063558cc6825279721d18a67fd5514c6555e5a
Michael Conrad <mmitton@gmail.com>
2024-10-18 04:58:59 -0400

    2015 Day 19

--- runner/aoc_2015/src/day_19.rs
+++ runner/aoc_2015/src/day_19.rs
@@ -2,25 +2,74 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
 #[derive(Default)]
-pub struct Day19 {}
+pub struct Day19 {
+    molecule: String,
+    rules: Vec<(String, String)>,
+}
 
 impl Day19 {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn expand(&self) -> usize {
+        let mut outputs = HashSet::default();
+        for (from, to) in self.rules.iter() {
+            let rule_len = from.len();
+            for i in 0..self.molecule.len() - rule_len + 1 {
+                if self.molecule[i..i + rule_len] == *from {
+                    let mut output = self.molecule[0..i].to_string();
+                    output.push_str(to);
+                    output.push_str(&self.molecule[i + rule_len..]);
+                    outputs.insert(output);
+                }
+            }
+        }
+        outputs.len()
+    }
+
+    fn reduce(&self) -> usize {
+        let mut molecule = self.molecule.clone();
+        let mut steps = 0;
+        while molecule != "e" {
+            for (from, to) in self.rules.iter() {
+                if from == "e" {
+                    if &molecule == to {
+                        steps += 1;
+                        molecule = from.into();
+                        break;
+                    }
+                } else {
+                    let new_molecule = molecule.replacen(to, from, 1);
+                    if new_molecule != molecule {
+                        molecule = new_molecule;
+                        steps += 1;
+                    }
+                }
+            }
+        }
+        steps
+    }
 }
 
 impl Runner for Day19 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::ALL)?;
+        for line in lines.iter() {
+            if let Some((from, to)) = line.split_once(" => ") {
+                self.rules.push((from.into(), to.into()));
+            } else {
+                self.molecule.push_str(line);
+            }
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.expand().into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.reduce().into())
     }
 }
