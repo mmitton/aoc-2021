115c140cca08a6ac046ecdc71bbbabab4fe64b49
Michael Conrad <mmitton@gmail.com>
2024-10-18 14:55:11 -0400

    2015 Day 24 - skip checks to see if the sleigh is full.  not needed for data.

--- runner/aoc_2015/src/day_24.rs
+++ runner/aoc_2015/src/day_24.rs
@@ -16,87 +16,56 @@ impl Day24 {
 
     fn calc_arrangements(&self, compartments: usize) -> Vec<(u32, usize)> {
         let compartment_weight = self.packages.iter().sum::<usize>() / compartments;
-        let mut builder: BTreeMap<usize, Vec<(u32, usize)>> = BTreeMap::new();
+        let mut builder: BTreeMap<usize, Vec<(u32, usize, usize)>> = BTreeMap::new();
         let mut found = BitArray::new(1 << self.packages.len());
 
         for (i, weight) in self.packages.iter().enumerate() {
-            builder.entry(*weight).or_default().push((1 << i, *weight));
+            builder
+                .entry(1)
+                .or_default()
+                .push((1 << i, *weight, *weight));
             found.set(1 << i, true);
         }
 
-        while let Some((total_weight, arrangements)) = builder.pop_first() {
-            if total_weight == compartment_weight {
-                return Vec::from_iter(arrangements.iter().copied());
-            }
-
+        let mut ans = Vec::new();
+        while let Some((num_packages, arrangements)) = builder.pop_first() {
             for (i, weight) in self.packages.iter().enumerate() {
-                let new_weight = total_weight + weight;
-                if new_weight > compartment_weight {
-                    continue;
-                }
                 let mask = 1 << i;
-                for (packages, qe) in arrangements.iter() {
+                for (packages, qe, total_weight) in arrangements.iter() {
+                    let new_weight = total_weight + weight;
+                    if new_weight > compartment_weight {
+                        continue;
+                    }
                     if packages & mask == 0 && !found.set((packages | mask) as usize, true) {
-                        builder
-                            .entry(new_weight)
-                            .or_default()
-                            .push((packages | mask, qe.saturating_mul(*weight)));
+                        if new_weight == compartment_weight {
+                            ans.push((packages | mask, qe.saturating_mul(*weight)));
+                        } else {
+                            builder.entry(num_packages + 1).or_default().push((
+                                packages | mask,
+                                qe.saturating_mul(*weight),
+                                new_weight,
+                            ));
+                        }
                     }
                 }
+            }
+
+            if !ans.is_empty() {
+                return ans;
             }
         }
 
         Vec::new()
     }
 
-    fn pack_sleigh(
-        &self,
-        passenger: u32,
-        arrangements: &[(u32, usize)],
-        compartments: usize,
-    ) -> bool {
-        let mut work = Vec::new();
-        work.push((1, passenger));
-
-        while let Some((cnt, packed)) = work.pop() {
-            for (packages, _) in arrangements.iter() {
-                if packed & packages == 0 {
-                    if cnt + 1 == compartments {
-                        return (packed | packages).count_ones() as usize == self.packages.len();
-                    }
-                    work.push((cnt + 1, packed | packages))
-                }
-            }
-        }
-        false
-    }
-
     fn run(&mut self, compartments: usize) -> usize {
         let arrangements = self.calc_arrangements(compartments);
-        println!(
-            "Found {} arrangements for {} compartments",
-            arrangements.len(),
-            compartments
-        );
-
-        for i in 0..self.packages.len() as u32 {
-            let starter: Vec<(u32, usize)> = arrangements
-                .iter()
-                .copied()
-                .filter(|(packages, _)| packages.count_ones() == i)
-                .collect();
-
-            let mut best = usize::MAX;
-            for (packages, qe) in starter.iter().copied() {
-                if self.pack_sleigh(packages, &arrangements, compartments) {
-                    best = best.min(qe);
-                }
-            }
-            if best != usize::MAX {
-                return best;
-            }
-        }
-        0
+        arrangements
+            .iter()
+            .copied()
+            .map(|(_, qe)| qe)
+            .min()
+            .unwrap()
     }
 }
 
