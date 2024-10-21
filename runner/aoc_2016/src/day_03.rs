dfc47435cfbd26001cec66c60ba9c46353622343
Michael Conrad <mmitton@gmail.com>
2024-10-21 04:14:59 -0400

    2016 Day 3

--- runner/aoc_2016/src/day_03.rs
+++ runner/aoc_2016/src/day_03.rs
@@ -2,7 +2,9 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
 #[derive(Default)]
-pub struct Day03 {}
+pub struct Day03 {
+    list: Vec<Vec<usize>>,
+}
 
 impl Day03 {
     pub fn new() -> Self {
@@ -12,15 +14,42 @@ impl Day03 {
 
 impl Runner for Day03 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        for line in lines.iter() {
+            self.list.push(
+                line.split_whitespace()
+                    .map(|n| n.parse().unwrap())
+                    .collect(),
+            );
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut possible = 0;
+        for nums in self.list.iter() {
+            if nums[0] + nums[1] > nums[2]
+                && nums[0] + nums[2] > nums[1]
+                && nums[1] + nums[2] > nums[0]
+            {
+                possible += 1;
+            }
+        }
+        Ok(possible.into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut possible = 0;
+        for nums in self.list.chunks(3) {
+            for x in 0..3 {
+                if nums[0][x] + nums[1][x] > nums[2][x]
+                    && nums[0][x] + nums[2][x] > nums[1][x]
+                    && nums[1][x] + nums[2][x] > nums[0][x]
+                {
+                    possible += 1;
+                }
+            }
+        }
+        Ok(possible.into())
     }
 }
