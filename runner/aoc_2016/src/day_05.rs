b375e72c67e566c05dd947a3e64ac5c90934512a
Michael Conrad <mmitton@gmail.com>
2024-10-21 05:25:08 -0400

    2016 Day 5

--- runner/aoc_2016/src/day_05.rs
+++ runner/aoc_2016/src/day_05.rs
@@ -1,8 +1,13 @@
 #[allow(unused_imports)]
-use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
+use helper::{
+    print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner, MD5,
+};
+use std::fmt::Write;
 
 #[derive(Default)]
-pub struct Day05 {}
+pub struct Day05 {
+    salt: String,
+}
 
 impl Day05 {
     pub fn new() -> Self {
@@ -12,15 +17,62 @@ impl Day05 {
 
 impl Runner for Day05 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        assert_eq!(lines.len(), 1);
+        self.salt = lines[0].clone();
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut password = String::with_capacity(8);
+        let mut hash = self.salt.clone();
+        for i in 0.. {
+            hash.truncate(self.salt.len());
+            write!(hash, "{i}")?;
+            let digest = MD5::digest(hash.as_bytes());
+            if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xF0 == 0 {
+                let v = digest[2] & 0xf;
+                password.push(if v < 10 {
+                    (v + b'0') as char
+                } else {
+                    ((v - 10) + b'a') as char
+                });
+                if password.len() == 8 {
+                    break;
+                }
+            }
+        }
+        Ok(password.into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut password = [None; 8];
+        let mut found = 0;
+        let mut hash = self.salt.clone();
+        for i in 0.. {
+            hash.truncate(self.salt.len());
+            write!(hash, "{i}")?;
+            let digest = MD5::digest(hash.as_bytes());
+            if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xF0 == 0 {
+                let i = digest[2] & 0xf;
+                let v = (digest[3] & 0xf0) >> 4;
+                if i < 8 && password[i as usize].is_none() {
+                    password[i as usize] = Some(if v < 10 {
+                        (v + b'0') as char
+                    } else {
+                        ((v - 10) + b'a') as char
+                    });
+                    found += 1;
+                    if found == 8 {
+                        break;
+                    }
+                }
+            }
+        }
+        Ok(password
+            .iter()
+            .map(|c| c.unwrap())
+            .collect::<String>()
+            .into())
     }
 }
