3cbe93c3c96b197d3b10cbe70bf0ddc9f09232c1
Michael Conrad <mmitton@gmail.com>
2024-10-23 07:23:54 -0400

    2016 Day 13

--- runner/aoc_2016/src/day_13.rs
+++ runner/aoc_2016/src/day_13.rs
@@ -2,25 +2,94 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
 #[derive(Default)]
-pub struct Day13 {}
+pub struct Day13 {
+    key: isize,
+}
 
 impl Day13 {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn is_open(&self, x: isize, y: isize) -> bool {
+        let num = (x * x) + (3 * x) + (2 * x * y) + y + (y * y);
+        let num = num + self.key;
+
+        let bits = num.count_ones();
+        bits % 2 == 0
+    }
+
+    fn find_paths<F>(&self, max_len: Option<usize>, stop_at: F) -> usize
+    where
+        F: Fn(isize, isize) -> bool,
+    {
+        let mut seen = std::collections::BTreeSet::new();
+        let mut paths = Vec::new();
+        let max_len = max_len.unwrap_or(usize::MAX);
+
+        let initial = (1isize, 1isize);
+        paths.push(vec![initial]);
+        seen.insert(initial);
+
+        let mut i = 0;
+        while i < paths.len() {
+            let last = paths[i][paths[i].len() - 1];
+            let x = last.0;
+            let y = last.1;
+
+            for dy in -1..=1 {
+                for dx in -1..=1 {
+                    if (dy == 0 && dx == 0) || (dy != 0 && dx != 0) {
+                        continue;
+                    }
+                    let x = x + dx;
+                    let y = y + dy;
+                    if x < 0 || y < 0 {
+                        continue;
+                    }
+
+                    if !self.is_open(x, y) {
+                        continue;
+                    }
+
+                    let next = (x, y);
+                    if seen.contains(&next) {
+                        continue;
+                    }
+
+                    let mut next_path = paths[i].clone();
+                    next_path.push(next);
+                    if stop_at(x, y) {
+                        return next_path.len() - 1;
+                    }
+
+                    seen.insert(next);
+                    if next_path.len() <= max_len {
+                        paths.push(next_path);
+                    }
+                }
+            }
+
+            i += 1;
+        }
+
+        seen.len()
+    }
 }
 
 impl Runner for Day13 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        assert_eq!(lines.len(), 1);
+        self.key = lines[0].parse()?;
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.find_paths(None, |x, y| x == 31 && y == 39).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.find_paths(Some(50), |_, _| false).into())
     }
 }
