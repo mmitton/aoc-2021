285a17bdf416f9e98daa8c19a7d88e12e0e95e71
Michael Conrad <mmitton@gmail.com>
2024-10-31 07:09:03 -0400

    2016 Day 16

--- runner/aoc_2016/src/day_16.rs
+++ runner/aoc_2016/src/day_16.rs
@@ -2,25 +2,66 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
 #[derive(Default)]
-pub struct Day16 {}
+pub struct Day16 {
+    initial_state: String,
+}
 
 impl Day16 {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn checksum(&self, len: usize) -> String {
+        let mut data = self.initial_state.clone();
+        while data.len() < len {
+            let chars: Vec<char> = data.chars().collect();
+            data.push('0');
+            for c in chars.iter().rev() {
+                match c {
+                    '1' => data.push('0'),
+                    '0' => data.push('1'),
+                    _ => unreachable!(),
+                }
+            }
+        }
+        let data = data[0..len].to_string();
+        let mut checksum = data.clone();
+        loop {
+            let mut new_checksum = String::new();
+            for i in (0..checksum.len()).step_by(2) {
+                match &checksum[i..i + 2] {
+                    "00" | "11" => new_checksum.push('1'),
+                    _ => new_checksum.push('0'),
+                }
+            }
+            checksum = new_checksum;
+
+            if checksum.len() % 2 == 1 {
+                break;
+            }
+        }
+        checksum
+    }
 }
 
 impl Runner for Day16 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        assert_eq!(lines.len(), 1);
+        self.initial_state.push_str(&lines[0]);
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let len = if self.initial_state.len() == 5 {
+            20
+        } else {
+            272
+        };
+        Ok(self.checksum(len).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.checksum(35651584).into())
     }
 }
