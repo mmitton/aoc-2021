22db6736c9410c02b440666b8af25a83e8428ed4
Michael Conrad <mmitton@gmail.com>
2024-10-31 15:08:34 -0400

    2016 Day 19

--- runner/aoc_2016/src/day_19.rs
+++ runner/aoc_2016/src/day_19.rs
@@ -1,8 +1,18 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
+#[derive(Debug)]
+struct Elf {
+    num: usize,
+    presents: usize,
+    next: usize,
+    prev: usize,
+}
+
 #[derive(Default)]
-pub struct Day19 {}
+pub struct Day19 {
+    elves: Vec<Elf>,
+}
 
 impl Day19 {
     pub fn new() -> Self {
@@ -12,15 +22,74 @@ impl Day19 {
 
 impl Runner for Day19 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        assert_eq!(lines.len(), 1);
+        let num: usize = lines[0].parse()?;
+
+        self.elves.extend((0..num).map(|num| Elf {
+            num: num + 1,
+            presents: 1,
+            next: num + 1,
+            prev: num.saturating_sub(1),
+        }));
+
+        self.elves[0].prev = num - 1;
+        self.elves[num - 1].next = 0;
+
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut cur = 0;
+        let mut elves_left = self.elves.len();
+        loop {
+            let next = self.elves[cur].next;
+            self.elves[cur].presents += self.elves[next].presents;
+            self.elves[next].presents = 0;
+
+            // Remove next
+            let prev = self.elves[next].prev;
+            let next = self.elves[next].next;
+            self.elves[prev].next = next;
+            self.elves[next].prev = prev;
+
+            elves_left -= 1;
+            cur = self.elves[cur].next;
+
+            if elves_left == 1 {
+                break;
+            }
+        }
+        Ok(self.elves[cur].num.into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        let mut cur = 0;
+        let mut across = self.elves.len() / 2;
+        let mut elves_left = self.elves.len();
+        loop {
+            self.elves[cur].presents += self.elves[across].presents;
+            self.elves[across].presents = 0;
+
+            // Remove across
+            let prev = self.elves[across].prev;
+            let next = self.elves[across].next;
+            self.elves[prev].next = next;
+            self.elves[next].prev = prev;
+
+            elves_left -= 1;
+            if elves_left % 2 == 0 {
+                across = self.elves[next].next;
+            } else {
+                across = next;
+            }
+
+            cur = self.elves[cur].next;
+
+            if elves_left == 1 {
+                break;
+            }
+        }
+        Ok(self.elves[cur].num.into())
     }
 }
