4d83596f72d82ab017ef19e6caa095b5ff2eec7c
Michael Conrad <mmitton@gmail.com>
2024-11-04 06:02:08 -0500

    2017 Day 1

--- runner/aoc_2017/src/day_01.rs
+++ runner/aoc_2017/src/day_01.rs
@@ -2,25 +2,42 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
 #[derive(Default)]
-pub struct Day01 {}
+pub struct Day01 {
+    data: Vec<u8>,
+}
 
 impl Day01 {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn captcha(&self, offset: usize) -> usize {
+        let mut sum = 0usize;
+        for i in 0..self.data.len() {
+            if self.data[i] == self.data[(i + offset) % self.data.len()] {
+                sum += self.data[i] as usize;
+            }
+        }
+
+        sum
+    }
 }
 
 impl Runner for Day01 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        assert_eq!(lines.len(), 1);
+        for c in lines[0].chars() {
+            self.data.push(c as u8 - b'0');
+        }
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.captcha(1).into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.captcha(self.data.len() / 2).into())
     }
 }
