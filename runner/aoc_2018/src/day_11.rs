a0da151438b893b1a87d26354e2362a7f55481da
Michael Conrad <mmitton@gmail.com>
2024-11-11 12:33:42 -0500

    2018 Day 11

--- runner/aoc_2018/src/day_11.rs
+++ runner/aoc_2018/src/day_11.rs
@@ -12,68 +12,6 @@ impl Day11 {
         Self::default()
     }
 
-    fn get_power_level(&self, x: usize, y: usize) -> isize {
-        let serial = self.serial as isize;
-        let x = x as isize;
-        let y = y as isize;
-
-        let rack_id = x + 10;
-        let rack_power_level = ((rack_id * y) + serial) * rack_id;
-        let power_level = (rack_power_level / 100) % 10;
-        power_level - 5
-    }
-
-    fn get_block_power_level(&self, x: usize, y: usize) -> isize {
-        let mut block_power_level = 0;
-        for x in x..x + 3 {
-            for y in y..y + 3 {
-                block_power_level += self.get_power_level(x, y);
-            }
-        }
-        block_power_level
-    }
-
-    fn find_best_block(&self, total_size: usize) -> (usize, usize, usize, isize) {
-        use std::collections::BTreeMap;
-        let mut power_levels = BTreeMap::new();
-
-        for x in 1..=total_size {
-            for y in 1..=total_size {
-                power_levels.insert((x, y), self.get_power_level(x, y));
-            }
-        }
-
-        let mut max_block_power_level = isize::MIN;
-        let mut max_x = 0;
-        let mut max_y = 0;
-        let mut max_size = 0;
-
-        for size in 1..=300 {
-            for sx in 1..=total_size - size + 1 {
-                for sy in 1..=total_size - size + 1 {
-                    let mut block_power_level = 0;
-                    for x in sx..sx + size {
-                        for y in sy..sy + size {
-                            block_power_level += self.get_power_level(x, y);
-                        }
-                    }
-                    if block_power_level > max_block_power_level {
-                        max_block_power_level = block_power_level;
-                        max_x = sx;
-                        max_y = sy;
-                        max_size = size;
-                    }
-                }
-            }
-            println!(
-                "{},{},{}  =  {}  ({})",
-                max_x, max_y, max_size, max_block_power_level, size
-            )
-        }
-
-        (max_x, max_y, max_size, max_block_power_level)
-    }
-
     fn get_summed_area(&self, x: isize, y: isize) -> isize {
         self.summed_area_table.get(&(x, y)).copied().unwrap_or(0)
     }
