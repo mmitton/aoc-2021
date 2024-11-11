7c6e5afa76e450c8c47a4574a9f5b1458f5980b5
Michael Conrad <mmitton@gmail.com>
2024-11-11 08:03:38 -0500

    2018 Day 8

--- runner/aoc_2018/src/day_08.rs
+++ runner/aoc_2018/src/day_08.rs
@@ -2,7 +2,47 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Output, RunOutput, Runner};
 
 #[derive(Default)]
-pub struct Day08 {}
+struct Node {
+    children: Vec<Node>,
+    meta: Vec<usize>,
+}
+
+impl Node {
+    fn parse<'a>(&mut self, mut data: &'a [usize]) -> &'a [usize] {
+        let num_children = data[0];
+        let num_meta = data[1];
+        data = &data[2..];
+        for _ in 0..num_children {
+            let mut child = Node::default();
+            data = child.parse(data);
+            self.children.push(child);
+        }
+        self.meta.extend(data.iter().take(num_meta));
+
+        &data[num_meta..]
+    }
+
+    fn meta_sum(&self) -> usize {
+        let children_sum = self.children.iter().map(|c| c.meta_sum()).sum::<usize>();
+        children_sum + self.meta.iter().sum::<usize>()
+    }
+
+    fn meta_sum2(&self) -> usize {
+        if self.children.is_empty() {
+            self.meta.iter().sum::<usize>()
+        } else {
+            self.meta
+                .iter()
+                .filter_map(|i| self.children.get(*i - 1).map(|child| child.meta_sum2()))
+                .sum::<usize>()
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct Day08 {
+    root: Node,
+}
 
 impl Day08 {
     pub fn new() -> Self {
@@ -12,15 +52,20 @@ impl Day08 {
 
 impl Runner for Day08 {
     fn parse(&mut self, file: &[u8], _part1: bool) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let data = lines[0]
+            .split_whitespace()
+            .map(|v| v.parse().unwrap())
+            .collect::<Vec<usize>>();
+        assert!(self.root.parse(&data).is_empty());
         Ok(())
     }
 
     fn part1(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.root.meta_sum().into())
     }
 
     fn part2(&mut self) -> Result<RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.root.meta_sum2().into())
     }
 }
