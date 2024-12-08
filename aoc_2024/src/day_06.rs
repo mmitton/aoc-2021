9508632fc9f591aecdb7beeeb14c8eb5d5859588
Michael Conrad <mmitton@gmail.com>
2024-12-08 04:26:28 -0500

    2024 Day 06 optimize

--- aoc_2024/src/day_06.rs
+++ aoc_2024/src/day_06.rs
@@ -16,78 +16,11 @@ struct Guard {
     dir: Dir,
 }
 
-impl Guard {
-    fn next_pos(&self) -> Point2D<u8> {
-        let (x, y) = match self.dir {
-            Dir::Up => (self.pos.x, self.pos.y.wrapping_sub(1)),
-            Dir::Right => (self.pos.x.wrapping_add(1), self.pos.y),
-            Dir::Down => (self.pos.x, self.pos.y.wrapping_add(1)),
-            Dir::Left => (self.pos.x.wrapping_sub(1), self.pos.y),
-        };
-        Point2D::new(x, y)
-    }
-
-    fn turn(&mut self) {
-        match self.dir {
-            Dir::Up => self.dir = Dir::Right,
-            Dir::Right => self.dir = Dir::Down,
-            Dir::Down => self.dir = Dir::Left,
-            Dir::Left => self.dir = Dir::Up,
-        }
-    }
-}
-
-#[derive(Copy, Clone, Default, Debug)]
-enum State {
-    #[default]
-    Empty,
-    Visited,
-    Obstruction,
-}
-
-#[derive(Clone, Default)]
-struct Lab {
-    guard: Guard,
-    map: Vec<Vec<State>>,
-    corners: HashSet<(Point2D<u8>, Point2D<u8>)>,
-}
-
-impl Lab {
-    fn map_path(&mut self) -> bool {
-        let mut next_pos = self.guard.next_pos();
-        while let Some(s) = self
-            .map
-            .get_mut(next_pos.y as usize)
-            .and_then(|r| r.get_mut(next_pos.x as usize))
-        {
-            match s {
-                State::Empty | State::Visited => {
-                    *s = State::Visited;
-                    self.guard.pos = next_pos;
-                }
-                State::Obstruction => {
-                    if !self.corners.insert((self.guard.pos, next_pos)) {
-                        return true;
-                    }
-                    self.guard.turn();
-                }
-            }
-            next_pos = self.guard.next_pos();
-        }
-        false
-    }
-
-    fn visited(&self) -> usize {
-        self.map
-            .iter()
-            .map(|row| row.iter().filter(|s| matches!(s, State::Visited)).count())
-            .sum::<usize>()
-    }
-}
-
 #[derive(Default)]
 pub struct Day06 {
-    lab: Lab,
+    guard: Guard,
+    rows: Vec<Vec<u8>>,
+    cols: Vec<Vec<u8>>,
 }
 
 impl Day06 {
@@ -95,52 +28,152 @@ impl Day06 {
         Self::default()
     }
 
+    fn walk_path<F>(&self, mut walked: F) -> bool
+    where
+        F: FnMut(Point2D<u8>, Point2D<u8>),
+    {
+        let mut guard = self.guard;
+        let mut seen = HashSet::default();
+        loop {
+            let (next_pos, dir, done) = match guard.dir {
+                Dir::Up => self.cols[guard.pos.x as usize]
+                    .iter()
+                    .copied()
+                    .map(|y| (Point2D::new(guard.pos.x, y), Dir::Right))
+                    .rev()
+                    .find(|(next_pos, _)| next_pos.y < guard.pos.y)
+                    .map(|(mut next_pos, dir)| {
+                        next_pos.y += 1;
+                        (next_pos, dir, false)
+                    })
+                    .unwrap_or((Point2D::new(guard.pos.x, 0), Dir::Up, true)),
+                Dir::Down => self.cols[guard.pos.x as usize]
+                    .iter()
+                    .copied()
+                    .map(|y| (Point2D::new(guard.pos.x, y), Dir::Left))
+                    .find(|(next_pos, _)| next_pos.y > guard.pos.y)
+                    .map(|(mut next_pos, dir)| {
+                        next_pos.y -= 1;
+                        (next_pos, dir, false)
+                    })
+                    .unwrap_or((
+                        Point2D::new(guard.pos.x, self.rows.len() as u8 - 1),
+                        Dir::Down,
+                        true,
+                    )),
+                Dir::Left => self.rows[guard.pos.y as usize]
+                    .iter()
+                    .copied()
+                    .map(|x| (Point2D::new(x, guard.pos.y), Dir::Up))
+                    .rev()
+                    .find(|(next_pos, _)| next_pos.x < guard.pos.x)
+                    .map(|(mut next_pos, dir)| {
+                        next_pos.x += 1;
+                        (next_pos, dir, false)
+                    })
+                    .unwrap_or((Point2D::new(0, guard.pos.y), Dir::Left, true)),
+                Dir::Right => self.rows[guard.pos.y as usize]
+                    .iter()
+                    .copied()
+                    .map(|x| (Point2D::new(x, guard.pos.y), Dir::Down))
+                    .find(|(next_pos, _)| next_pos.x > guard.pos.x)
+                    .map(|(mut next_pos, dir)| {
+                        next_pos.x -= 1;
+                        (next_pos, dir, false)
+                    })
+                    .unwrap_or((
+                        Point2D::new(self.cols.len() as u8 - 1, guard.pos.y),
+                        Dir::Left,
+                        true,
+                    )),
+            };
+            if !seen.insert((guard.pos, next_pos)) {
+                return true;
+            }
+            walked(guard.pos, next_pos);
+            guard.pos = next_pos;
+            guard.dir = dir;
+            if done {
+                return false;
+            }
+        }
+    }
+
+    fn initial_walked(&self) -> HashSet<Point2D<u8>> {
+        let mut walked = HashSet::default();
+        assert!(!self.walk_path(|from, to| {
+            if from.x != to.x && from.y == to.y {
+                let min_x = from.x.min(to.x);
+                let max_x = from.x.max(to.x);
+                for x in min_x..=max_x {
+                    walked.insert(Point2D::new(x, from.y));
+                }
+            } else if from.y != to.y && from.x == to.x {
+                let min_y = from.y.min(to.y);
+                let max_y = from.y.max(to.y);
+                for y in min_y..=max_y {
+                    walked.insert(Point2D::new(from.x, y));
+                }
+            } else {
+                unreachable!()
+            }
+        }));
+        walked
+    }
+
     fn part1(&mut self) -> Result<helper::RunOutput, Error> {
-        self.lab.map_path();
-        Ok(self.lab.visited().into())
+        let walked = self.initial_walked();
+        Ok(walked.len().into())
     }
 
     fn part2(&mut self) -> Result<helper::RunOutput, Error> {
-        let mut initial_lab = self.lab.clone();
-        initial_lab.map_path();
-        let mut ans = 0;
-        for y in 0..self.lab.map.len() {
-            for x in 0..self.lab.map[y].len() {
-                if x == self.lab.guard.pos.x as usize && y == self.lab.guard.pos.y as usize {
-                    continue;
+        Ok(self
+            .initial_walked()
+            .iter()
+            .copied()
+            .filter(|try_at| {
+                if *try_at == self.guard.pos {
+                    false
+                } else {
+                    self.rows[try_at.y as usize].push(try_at.x);
+                    self.cols[try_at.x as usize].push(try_at.y);
+                    self.rows[try_at.y as usize].sort();
+                    self.cols[try_at.x as usize].sort();
+
+                    let is_loop = self.walk_path(|_, _| {});
+
+                    self.rows[try_at.y as usize].retain(|x| *x != try_at.x);
+                    self.cols[try_at.x as usize].retain(|y| *y != try_at.y);
+
+                    is_loop
                 }
-                if matches!(initial_lab.map[y][x], State::Visited) {
-                    let mut lab = self.lab.clone();
-                    lab.map[y][x] = State::Obstruction;
-                    if lab.map_path() {
-                        ans += 1;
-                    }
-                }
-            }
-        }
-        Ok(ans.into())
+            })
+            .count()
+            .into())
     }
 }
 
 impl helper::Runner for Day06 {
     fn parse(&mut self, file: &[u8], _part: u8) -> Result<(), Error> {
         let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        self.rows.resize(lines.len(), Vec::new());
+        self.cols.resize(lines[0].len(), Vec::new());
         for (y, line) in lines.iter().enumerate() {
-            let mut row = Vec::new();
             for (x, c) in line.chars().enumerate() {
-                row.push(match c {
-                    '.' => State::Empty,
-                    '#' => State::Obstruction,
+                match c {
+                    '.' => {}
+                    '#' => {
+                        self.rows[y].push(x as u8);
+                        self.cols[x].push(y as u8);
+                    }
                     '^' => {
-                        self.lab.guard.pos.x = x as u8;
-                        self.lab.guard.pos.y = y as u8;
-                        self.lab.guard.dir = Dir::Up;
-                        State::Visited
+                        self.guard.pos.x = x as u8;
+                        self.guard.pos.y = y as u8;
+                        self.guard.dir = Dir::Up;
                     }
                     _ => unreachable!(),
-                });
+                }
             }
-            self.lab.map.push(row);
         }
         Ok(())
     }
