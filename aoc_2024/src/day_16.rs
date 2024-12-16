889156ce963657c696bb92838460b01c25a85758
Michael Conrad <mmitton@gmail.com>
2024-12-16 10:16:18 -0500

    2024 Day 16 - Convert to generic dijkstra algo in helper, which can now return the paths too!

--- aoc_2024/src/day_16.rs
+++ aoc_2024/src/day_16.rs
@@ -1,6 +1,5 @@
 #[allow(unused_imports)]
-use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Point2D};
-use std::collections::BTreeMap;
+use helper::{print, println, Dijkstra, Error, HashMap, HashSet, Lines, LinesOpt, Point2D};
 
 #[derive(Default)]
 pub struct Day16 {
@@ -50,79 +49,24 @@ impl Day16 {
     }
 
     fn walk(&self) -> Result<(usize, usize), Error> {
-        let mut work: BTreeMap<usize, HashSet<(Point2D<usize>, Dir)>> = BTreeMap::new();
-        work.entry(0).or_default().insert((self.start, Dir::East));
-
-        #[derive(Debug)]
-        struct SeenEntry {
-            cost: usize,
-            from: HashSet<(Point2D<usize>, Dir)>,
-        }
-
-        let mut seen: HashMap<(Point2D<usize>, Dir), SeenEntry> = HashMap::default();
-        seen.insert(
-            (self.start, Dir::East),
-            SeenEntry {
-                cost: 0,
-                from: HashSet::default(),
-            },
-        );
-
-        let mut walked_along = HashSet::default();
-        while let Some((cost, mut cost_work)) = work.pop_first() {
-            for (cur, dir) in cost_work.drain() {
-                if cur == self.end {
-                    let mut work = vec![(self.end, dir)];
-                    walked_along.insert(self.end);
-
-                    while let Some((at, dir)) = work.pop() {
-                        if let Some(seen_entry) = seen.get(&(at, dir)) {
-                            for (from, from_dir) in seen_entry.from.iter() {
-                                walked_along.insert(*from);
-                                work.push((*from, *from_dir));
-                            }
-                        }
-                    }
-                    continue;
+        let (cost, paths) = Dijkstra::find_first_paths((self.start, Dir::East), |(at, dir)| {
+            dir.next(at).into_iter().filter_map(|(cost, (at, dir))| {
+                if self.hall.contains(&at) {
+                    Some((cost, (at, dir), at == self.end))
+                } else {
+                    None
                 }
-                if let Some(seen_cost) = seen.get(&(cur, dir)) {
-                    if seen_cost.cost < cost {
-                        continue;
-                    }
-                }
-                for (next_cost, (next, next_dir)) in dir.next(cur) {
-                    if !self.hall.contains(&next) {
-                        continue;
-                    }
+            })
+        })
+        .ok_or(Error::Unsolved)?;
 
-                    let next_cost = cost + next_cost;
-                    let seen_cost = seen.entry((next, next_dir)).or_insert(SeenEntry {
-                        cost: usize::MAX,
-                        from: HashSet::default(),
-                    });
-                    use std::cmp::Ordering;
-                    match seen_cost.cost.cmp(&next_cost) {
-                        Ordering::Less => continue,
-                        Ordering::Equal => {
-                            seen_cost.from.insert((cur, dir));
-                            continue;
-                        }
-                        Ordering::Greater => {
-                            seen_cost.cost = next_cost;
-                            seen_cost.from.clear();
-                            seen_cost.from.insert((cur, dir));
-                        }
-                    }
-                    work.entry(next_cost).or_default().insert((next, next_dir));
-                }
-            }
-
-            if !walked_along.is_empty() {
-                return Ok((cost, walked_along.len()));
+        let mut points = HashSet::default();
+        for path in paths.iter() {
+            for (point, _) in path.iter() {
+                points.insert(point);
             }
         }
-
-        Err(Error::Unsolved)
+        Ok((cost, points.len()))
     }
 
     fn part1(&mut self) -> Result<helper::RunOutput, Error> {
