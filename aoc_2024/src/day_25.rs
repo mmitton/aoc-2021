d0b7289e86ef6e2c8a48f69ff1d153eb9ba05ffd
Michael Conrad <mmitton@gmail.com>
2024-12-25 01:25:28 -0500

    2024 Day 25

--- aoc_2024/src/day_25.rs
+++ aoc_2024/src/day_25.rs
@@ -2,7 +2,10 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt};
 
 #[derive(Default)]
-pub struct Day25 {}
+pub struct Day25 {
+    locks: Vec<[usize; 5]>,
+    keys: Vec<[usize; 5]>,
+}
 
 impl Day25 {
     pub fn new() -> Self {
@@ -10,13 +13,57 @@ impl Day25 {
     }
 
     fn part1(&mut self) -> Result<helper::RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self
+            .locks
+            .iter()
+            .map(|lock| {
+                self.keys
+                    .iter()
+                    .filter(|key| !lock.iter().zip(key.iter()).any(|(l, k)| l + k > 5))
+                    .count()
+            })
+            .sum::<usize>()
+            .into())
     }
 }
 
 impl helper::Runner for Day25 {
     fn parse(&mut self, file: &[u8], _part: u8) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::REMOVE_EMPTY)?;
+        for lines in lines.chunks(7) {
+            let (lines, is_key): (Vec<Vec<char>>, bool) = if lines[0] == "#####" {
+                (
+                    lines.iter().skip(1).map(|l| l.chars().collect()).collect(),
+                    false,
+                )
+            } else {
+                (
+                    lines
+                        .iter()
+                        .rev()
+                        .skip(1)
+                        .map(|l| l.chars().collect())
+                        .collect(),
+                    true,
+                )
+            };
+
+            let mut heights = [0; 5];
+            for x in 0..5 {
+                for y in (0..6).rev() {
+                    if lines[y][x] == '#' {
+                        heights[x] = y + 1;
+                        break;
+                    }
+                }
+            }
+
+            if is_key {
+                self.keys.push(heights);
+            } else {
+                self.locks.push(heights);
+            }
+        }
         Ok(())
     }
 
